a5790cb1b7867a987eccd064d02bcf1d9b388596
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-18 09:37:38 +0430

    service template of Anar Framework

---
+++ src/main/java/af/gov/anar/anarservicetemplate/AnarServicetemplateApplication.java
@@ -0,0 +1,13 @@
+package af.gov.anar.anarservicetemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnarServicetemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnarServicetemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/af/gov/anar/anarservicetemplate/AnarServicetemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package af.gov.anar.anarservicetemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnarServicetemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
