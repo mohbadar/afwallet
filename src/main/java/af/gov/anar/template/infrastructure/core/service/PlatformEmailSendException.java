983a40287efaf2c273f745a47f0dc54f81db36de
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-26 10:20:56 +0430

    refactor

--- src/main/java/af/gov/anar/template/infrastructure/core/service/PlatformEmailSendException.java
+++
@@ -1,9 +0,0 @@
-
-package af.gov.anar.template.infrastructure.core.service;
-
-public class PlatformEmailSendException extends RuntimeException {
-
-    public PlatformEmailSendException(final Throwable e) {
-        super(e);
-    }
-}
