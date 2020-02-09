b673b7a207df9899f3ec66416a235573adb237af
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-09 13:32:04 +0430

    controller complaint

--- src/main/java/af/gov/anar/ebreshna/helpdesk/controller/ComplaintController.java
+++ src/main/java/af/gov/anar/ebreshna/helpdesk/controller/ComplaintController.java
@@ -1,9 +1,47 @@
 package af.gov.anar.ebreshna.helpdesk.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.helpdesk.model.Complaint;
+import af.gov.anar.ebreshna.helpdesk.service.ComplaintService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/helpdesk/complaints")
 public class ComplaintController {
+
+    @Autowired
+    private ComplaintService service;
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<Complaint>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<Complaint> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<Complaint> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) Complaint obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<Complaint> save(@RequestBody(required = true) Complaint obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
