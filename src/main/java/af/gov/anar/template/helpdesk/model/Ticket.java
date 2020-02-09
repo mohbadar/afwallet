8780727da99867c479368d82cdf27faae8e64806
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-09 10:39:48 +0430

    ticket

---
+++ src/main/java/af/gov/anar/template/helpdesk/model/Ticket.java
@@ -0,0 +1,30 @@
+package af.gov.anar.template.helpdesk.model;
+
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "ticket")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+@Audited
+public class Ticket{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+    private String content;
+    private String department;
+    private String file;
+    private Date date;
+}
