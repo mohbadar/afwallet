34dc7eba6b3f37e5627bf74850500259d3c37954
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-09 13:15:42 +0430

    complaint service

--- src/main/java/af/gov/anar/ebreshna/helpdesk/service/ComplaintService.java
+++ src/main/java/af/gov/anar/ebreshna/helpdesk/service/ComplaintService.java
@@ -1,4 +1,44 @@
 package af.gov.anar.ebreshna.helpdesk.service;
 
+import af.gov.anar.ebreshna.helpdesk.model.Comment;
+import af.gov.anar.ebreshna.helpdesk.model.Complaint;
+import af.gov.anar.ebreshna.helpdesk.repository.CommentRepository;
+import af.gov.anar.ebreshna.helpdesk.repository.ComplaintRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.List;
+
 public class ComplaintService {
+
+    @Autowired
+    private ComplaintRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public Complaint save(Complaint obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<Complaint> findall()
+    {
+        return repository.findAll();
+    }
+
+    public Complaint findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        Complaint obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
 }
