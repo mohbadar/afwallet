45b9c464909cdada4974dcc8e5688223d45117fe
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-09 13:30:48 +0430

    complaint api model

---
+++ src/main/java/af/gov/anar/ebreshna/helpdesk/controller/ComplaintController.java
@@ -0,0 +1,9 @@
+package af.gov.anar.ebreshna.helpdesk.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/helpdesk/complaints")
+public class ComplaintController {
+}
