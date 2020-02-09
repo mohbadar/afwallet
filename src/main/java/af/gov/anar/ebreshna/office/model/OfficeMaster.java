20b94d3b84cb71f928dd88cef82a4be08a83d47e
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-09 12:05:53 +0430

    office master

---
+++ src/main/java/af/gov/anar/ebreshna/office/model/OfficeMaster.java
@@ -0,0 +1,42 @@
+package af.gov.anar.ebreshna.office.model;
+
+import af.gov.anar.ebreshna.common.model.BaseEntity;
+import af.gov.anar.ebreshna.infrastructure.util.Schema;
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "office_master", schema = Schema.CORE_SCHEMA)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+@Audited
+public class OfficeMaster extends BaseEntity {
+
+    @ManyToOne(targetEntity = OfficeType.class)
+    private OfficeType officeType;
+
+    @OneToOne(cascade={CascadeType.ALL})
+    @JoinColumn(name="parent")
+    private OfficeMaster officeMaster;
+
+    private String name;
+    private String officeCode;
+    private String shortName;
+    private String doorNumber;
+    private String street;
+    private String city;
+    private String phone1;
+    private String phone2;
+    private String phone3;
+    private String fax;
+    private String email;
+//    private
+
+}
