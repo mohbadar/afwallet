e290b512f8560b8903080dc10ea7fc6b105719ae
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-09 14:37:27 +0430

    premises nature

---
+++ src/main/java/af/gov/anar/ebreshna/office/model/PremisesSubCategory.java
@@ -0,0 +1,21 @@
+package af.gov.anar.ebreshna.office.model;
+
+import af.gov.anar.ebreshna.infrastructure.util.Schema;
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "crm_premises_sub_category", schema = Schema.CORE_SCHEMA)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+@Audited
+public class PremisesSubCategory {
+}
