90057fe6e885949d03c8ba065ba37f7fd10e32e1
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 17:11:06 +0430

    meter reader controller

--- src/main/java/af/gov/anar/ebreshna/common/network/controller/MeterReaderController.java
+++ src/main/java/af/gov/anar/ebreshna/common/network/controller/MeterReaderController.java
@@ -1,9 +1,55 @@
 package af.gov.anar.ebreshna.common.network.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.common.network.model.MeterReaderMaster;
+import af.gov.anar.ebreshna.common.network.service.MeterReaderMasterService;
+import af.gov.anar.ebreshna.common.office.model.DesignationMaster;
+import af.gov.anar.ebreshna.common.office.service.DesignationMasterService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/config/network/meterreaders")
 public class MeterReaderController {
+
+    @Autowired
+    private MeterReaderMasterService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<MeterReaderMaster>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<MeterReaderMaster> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<MeterReaderMaster> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) MeterReaderMaster obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<MeterReaderMaster> save(@RequestBody(required = true) MeterReaderMaster obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
