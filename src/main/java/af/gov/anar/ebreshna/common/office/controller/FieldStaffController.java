cddad5b6a07ca57159a4862799d8e1cf034906d1
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 13:45:00 +0430

    field staff api controller

--- src/main/java/af/gov/anar/ebreshna/common/office/controller/FieldStaffController.java
+++ src/main/java/af/gov/anar/ebreshna/common/office/controller/FieldStaffController.java
@@ -1,9 +1,55 @@
 package af.gov.anar.ebreshna.common.office.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.common.office.model.DesignationMaster;
+import af.gov.anar.ebreshna.common.office.model.FieldStaff;
+import af.gov.anar.ebreshna.common.office.service.DesignationMasterService;
+import af.gov.anar.ebreshna.common.office.service.FieldStaffService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/config/office/fieldstaff")
 public class FieldStaffController {
+
+    @Autowired
+    private FieldStaffService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<FieldStaff>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<FieldStaff> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<FieldStaff> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) FieldStaff obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<FieldStaff> save(@RequestBody(required = true) FieldStaff obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
