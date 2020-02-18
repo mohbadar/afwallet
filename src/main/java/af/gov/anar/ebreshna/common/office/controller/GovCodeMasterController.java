67158c5d16e2ed97ddd5505e0efc53ade87d3256
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 13:46:02 +0430

    gov code master api controller

--- src/main/java/af/gov/anar/ebreshna/common/office/controller/GovCodeMasterController.java
+++ src/main/java/af/gov/anar/ebreshna/common/office/controller/GovCodeMasterController.java
@@ -1,9 +1,56 @@
 package af.gov.anar.ebreshna.common.office.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.api.handler.ResponseHandler;
+import af.gov.anar.ebreshna.common.office.model.DesignationMaster;
+import af.gov.anar.ebreshna.common.office.model.GovCodeMaster;
+import af.gov.anar.ebreshna.common.office.service.DesignationMasterService;
+import af.gov.anar.ebreshna.common.office.service.GovCodeMasterService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/config/office/govcodes")
-public class GovCodeMasterController {
+public class GovCodeMasterController extends ResponseHandler {
+
+    @Autowired
+    private GovCodeMasterService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<GovCodeMaster>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<GovCodeMaster> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<GovCodeMaster> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) GovCodeMaster obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<GovCodeMaster> save(@RequestBody(required = true) GovCodeMaster obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
