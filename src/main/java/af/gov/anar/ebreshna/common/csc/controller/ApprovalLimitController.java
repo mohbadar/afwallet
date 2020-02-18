45f18ed868965424e841346cc5d42e544d879f39
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 17:21:30 +0430

    csc api model

---
+++ src/main/java/af/gov/anar/ebreshna/common/csc/controller/ApprovalLimitController.java
@@ -0,0 +1,10 @@
+package af.gov.anar.ebreshna.common.csc.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/config/csc/approvallimits")
+public class ApprovalLimitController {
+}
