b517571c7436905ded6e05f29c7427e58295fa7d
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 16:12:00 +0430

    voltage level repo

--- src/main/java/af/gov/anar/ebreshna/common/network/repository/VoltageLevelRepository.java
+++ src/main/java/af/gov/anar/ebreshna/common/network/repository/VoltageLevelRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.common.network.repository;
 
-public interface VoltageLevelRepository {
+import af.gov.anar.ebreshna.common.network.model.AreaMaster;
+import af.gov.anar.ebreshna.common.network.model.VoltageLevel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoltageLevelRepository extends JpaRepository<VoltageLevel, Long> {
 }
