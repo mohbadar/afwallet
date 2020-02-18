3a0936b4b443fe56bff38405a323bd66b2886d28
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 16:53:52 +0430

    ptr master service

--- src/main/java/af/gov/anar/ebreshna/common/network/repository/PTRMasterRepository.java
+++ src/main/java/af/gov/anar/ebreshna/common/network/repository/PTRMasterRepository.java
@@ -2,9 +2,10 @@ package af.gov.anar.ebreshna.common.network.repository;
 
 import af.gov.anar.ebreshna.common.network.model.AreaMaster;
 import af.gov.anar.ebreshna.common.network.model.MeterReaderMaster;
+import af.gov.anar.ebreshna.common.network.model.PTRMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PRTMasterRepository extends JpaRepository<MeterReaderMaster, Long> {
+public interface PTRMasterRepository extends JpaRepository<PTRMaster, Long> {
 }
--- src/main/java/af/gov/anar/ebreshna/common/network/service/PTRMasterService.java
+++ src/main/java/af/gov/anar/ebreshna/common/network/service/PTRMasterService.java
@@ -1,4 +1,46 @@
 package af.gov.anar.ebreshna.common.network.service;
 
+import af.gov.anar.ebreshna.common.network.model.PTRMaster;
+import af.gov.anar.ebreshna.common.network.repository.PTRMasterRepository;
+import af.gov.anar.ebreshna.common.office.model.DesignationMaster;
+import af.gov.anar.ebreshna.common.office.repository.DesignationMasterRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class PTRMasterService {
+
+    @Autowired
+    private PTRMasterRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public PTRMaster save(PTRMaster obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<PTRMaster> findall()
+    {
+        return repository.findAll();
+    }
+
+    public PTRMaster findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        PTRMaster obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
 }
