35a74a13d114de3082ad26577a67137642463457
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 13:52:51 +0430

    office master api controller

--- src/main/java/af/gov/anar/ebreshna/common/office/controller/OfficeMasterController.java
+++ src/main/java/af/gov/anar/ebreshna/common/office/controller/OfficeMasterController.java
@@ -1,9 +1,56 @@
 package af.gov.anar.ebreshna.common.office.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.api.handler.ResponseHandler;
+import af.gov.anar.ebreshna.common.office.model.DesignationMaster;
+import af.gov.anar.ebreshna.common.office.model.OfficeMaster;
+import af.gov.anar.ebreshna.common.office.service.DesignationMasterService;
+import af.gov.anar.ebreshna.common.office.service.OfficeMasterService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/config/office/officemasters")
-public class OfficeMasterController {
+public class OfficeMasterController extends ResponseHandler {
+
+    @Autowired
+    private OfficeMasterService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<OfficeMaster>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<OfficeMaster> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<OfficeMaster> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) OfficeMaster obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<OfficeMaster> save(@RequestBody(required = true) OfficeMaster obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
