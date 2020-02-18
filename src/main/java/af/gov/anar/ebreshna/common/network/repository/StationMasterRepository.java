ec418e946c4336ddc6e9a7aa590cd8e552e08ce1
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 16:10:40 +0430

    station master repo

--- src/main/java/af/gov/anar/ebreshna/common/network/repository/StationMasterRepository.java
+++ src/main/java/af/gov/anar/ebreshna/common/network/repository/StationMasterRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.common.network.repository;
 
-public interface StationMasterRepository {
+import af.gov.anar.ebreshna.common.network.model.AreaMaster;
+import af.gov.anar.ebreshna.common.network.model.StationMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StationMasterRepository extends JpaRepository<StationMaster, Long> {
 }
