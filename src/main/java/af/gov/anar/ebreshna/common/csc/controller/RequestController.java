d5378160fb4443c66bddbb47e01864256678f6fd
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 17:29:29 +0430

    request controller

--- src/main/java/af/gov/anar/ebreshna/common/csc/controller/RequestController.java
+++ src/main/java/af/gov/anar/ebreshna/common/csc/controller/RequestController.java
@@ -1,9 +1,56 @@
 package af.gov.anar.ebreshna.common.csc.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.common.csc.model.Request;
+import af.gov.anar.ebreshna.common.csc.service.RequestService;
+import af.gov.anar.ebreshna.common.network.model.AreaMaster;
+import af.gov.anar.ebreshna.common.network.service.AreaMasterService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.bouncycastle.cert.ocsp.Req;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/config/csc/requests")
 public class RequestController {
+
+    @Autowired
+    private RequestService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<Request>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<Request> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<Request> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) Request obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<Request> save(@RequestBody(required = true) Request obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
