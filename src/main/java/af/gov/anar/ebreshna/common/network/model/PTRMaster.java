c456929862d77687443f131c95c916b30d63f090
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 13:08:41 +0430

    ptr master

--- src/main/java/af/gov/anar/ebreshna/common/network/model/PTRMaster.java
+++ src/main/java/af/gov/anar/ebreshna/common/network/model/PTRMaster.java
@@ -1,4 +1,53 @@
 package af.gov.anar.ebreshna.common.network.model;
 
-public class PTRMaster {
+
+import af.gov.anar.ebreshna.common.base.BaseEntity;
+import af.gov.anar.ebreshna.common.office.model.OfficeMaster;
+import af.gov.anar.ebreshna.infrastructure.util.Schema;
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "network_ptr_master", schema = Schema.CORE_SCHEMA)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+@Audited
+public class PTRMaster extends BaseEntity {
+
+    @ManyToOne(targetEntity = SubstationMaster.class)
+    private SubstationMaster substationMaster;
+
+    @ManyToOne(targetEntity = FeederMaster.class)
+    private FeederMaster feederMaster;
+
+
+    @Column
+    private String ptrName;
+
+    @Column
+    private String ptrCode;
+
+
+    @Column
+    private String primaryVoltage;
+
+    @Column
+    private String secondaryVoltage;
+
+    @Column
+    private String ptrCapacity;
+
+    @Column
+    private String ain;
+
 }
