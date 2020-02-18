969f84611d9e83b50da500244ee3b2572edf3b43
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 16:11:06 +0430

    station type

--- src/main/java/af/gov/anar/ebreshna/common/network/repository/StationTypeRepository.java
+++ src/main/java/af/gov/anar/ebreshna/common/network/repository/StationTypeRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.common.network.repository;
 
-public interface StationTypeRepository {
+import af.gov.anar.ebreshna.common.network.model.AreaMaster;
+import af.gov.anar.ebreshna.common.network.model.StationType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StationTypeRepository extends JpaRepository<StationType, Long> {
 }
