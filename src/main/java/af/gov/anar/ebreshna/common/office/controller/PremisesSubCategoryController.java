9617ae82f514e2f9f1970671bc2edd14c0e3d234
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 16:01:53 +0430

    premises sub category

--- src/main/java/af/gov/anar/ebreshna/common/office/controller/PremisesSubCategoryController.java
+++ src/main/java/af/gov/anar/ebreshna/common/office/controller/PremisesSubCategoryController.java
@@ -1,9 +1,55 @@
 package af.gov.anar.ebreshna.common.office.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.common.office.model.OfficeType;
+import af.gov.anar.ebreshna.common.office.model.PremisesSubCategory;
+import af.gov.anar.ebreshna.common.office.service.OfficeTypeService;
+import af.gov.anar.ebreshna.common.office.service.PremisesSubCategoryService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/config/office/premisessubcategories")
 public class PremisesSubCategoryController {
+
+    @Autowired
+    private PremisesSubCategoryService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<PremisesSubCategory>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<PremisesSubCategory> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<PremisesSubCategory> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) PremisesSubCategory obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<PremisesSubCategory> save(@RequestBody(required = true) PremisesSubCategory obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
