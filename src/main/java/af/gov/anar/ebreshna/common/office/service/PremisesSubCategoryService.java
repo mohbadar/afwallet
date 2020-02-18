64c5b83242c28982484e0c950c92a89551387c5b
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 13:22:17 +0430

    premises sub category service

--- src/main/java/af/gov/anar/ebreshna/common/office/service/PremisesSubCategoryService.java
+++ src/main/java/af/gov/anar/ebreshna/common/office/service/PremisesSubCategoryService.java
@@ -1,4 +1,46 @@
 package af.gov.anar.ebreshna.common.office.service;
 
+import af.gov.anar.ebreshna.common.office.model.PremisesSubCategory;
+import af.gov.anar.ebreshna.common.office.repository.PremisesSubCategoryRepository;
+import af.gov.anar.ebreshna.helpdesk.model.Comment;
+import af.gov.anar.ebreshna.helpdesk.repository.CommentRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class PremisesSubCategoryService {
+
+    @Autowired
+    private PremisesSubCategoryRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public PremisesSubCategory save(PremisesSubCategory obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<PremisesSubCategory> findall()
+    {
+        return repository.findAll();
+    }
+
+    public PremisesSubCategory findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        PremisesSubCategory obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
 }
