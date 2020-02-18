8e1fded7e528c32f2ece3bb83f46b483428f0f08
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 16:48:08 +0430

    area master service

--- src/main/java/af/gov/anar/ebreshna/common/network/service/AreaMasterService.java
+++ src/main/java/af/gov/anar/ebreshna/common/network/service/AreaMasterService.java
@@ -1,4 +1,46 @@
 package af.gov.anar.ebreshna.common.network.service;
 
+import af.gov.anar.ebreshna.common.network.model.AreaMaster;
+import af.gov.anar.ebreshna.common.network.repository.AreaMasterRepository;
+import af.gov.anar.ebreshna.common.office.model.DesignationMaster;
+import af.gov.anar.ebreshna.common.office.repository.DesignationMasterRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class AreaMasterService {
+
+    @Autowired
+    private AreaMasterRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public AreaMaster save(AreaMaster obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<AreaMaster> findall()
+    {
+        return repository.findAll();
+    }
+
+    public AreaMaster findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        AreaMaster obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
 }
