579f7492c6655489d345fcb3708e4ae62463c2db
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 16:09:43 +0430

    meter reader repos

--- src/main/java/af/gov/anar/ebreshna/common/network/repository/MeterReaderMasterRepository.java
+++ src/main/java/af/gov/anar/ebreshna/common/network/repository/MeterReaderMasterRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.common.network.repository;
 
-public interface MeterReaderMasterRepository {
+import af.gov.anar.ebreshna.common.network.model.AreaMaster;
+import af.gov.anar.ebreshna.common.network.model.MeterReaderMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeterReaderMasterRepository extends JpaRepository<MeterReaderMaster, Long> {
 }
