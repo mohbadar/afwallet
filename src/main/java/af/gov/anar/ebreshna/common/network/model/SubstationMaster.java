4be20589013b3987ce35c330de53e9004c79519b
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 11:22:40 +0430

    substation

--- src/main/java/af/gov/anar/ebreshna/common/network/model/SubstationMaster.java
+++ src/main/java/af/gov/anar/ebreshna/common/network/model/SubstationMaster.java
@@ -1,4 +1,42 @@
 package af.gov.anar.ebreshna.common.network.model;
 
-public class SubstationMaster {
+import af.gov.anar.ebreshna.common.base.BaseEntity;
+import af.gov.anar.ebreshna.common.network.enumeration.SubstationCapacity;
+import af.gov.anar.ebreshna.common.office.model.DesignationMaster;
+import af.gov.anar.ebreshna.common.office.model.OfficeMaster;
+import af.gov.anar.ebreshna.common.province.Province;
+import af.gov.anar.ebreshna.infrastructure.util.Schema;
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "network_substation_master", schema = Schema.CORE_SCHEMA)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+@Audited
+public class SubstationMaster extends BaseEntity {
+
+    @ManyToOne(targetEntity = OfficeMaster.class)
+    private OfficeMaster officeMaster;
+
+    @Column
+    private SubstationCapacity substationCapacity;
+
+    @Column
+    private String stationName;
+    @Column
+    private String stationCode;
+    @Column
+    private String stationLocation;
+
 }
