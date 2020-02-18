16e9828f8476b507f8320b261b30a59066132cfd
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 13:15:31 +0430

    designation service

--- src/main/java/af/gov/anar/ebreshna/common/office/service/DesignationMasterService.java
+++ src/main/java/af/gov/anar/ebreshna/common/office/service/DesignationMasterService.java
@@ -1,4 +1,47 @@
 package af.gov.anar.ebreshna.common.office.service;
 
+import af.gov.anar.ebreshna.common.office.model.DesignationMaster;
+import af.gov.anar.ebreshna.common.office.repository.DesignationMasterRepository;
+import af.gov.anar.ebreshna.helpdesk.model.Comment;
+import af.gov.anar.ebreshna.helpdesk.repository.CommentRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import sun.security.krb5.internal.crypto.Des;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class DesignationMasterService {
+
+    @Autowired
+    private DesignationMasterRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public DesignationMaster save(DesignationMaster obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<DesignationMaster> findall()
+    {
+        return repository.findAll();
+    }
+
+    public DesignationMaster findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        DesignationMaster obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
 }
