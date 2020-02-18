d87e0b8ab0f98eb7467a69753a9102aeeb3d99b1
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-18 16:07:50 +0430

    area master repos

--- src/main/java/af/gov/anar/ebreshna/common/network/repository/AreaMasterRepository.java
+++ src/main/java/af/gov/anar/ebreshna/common/network/repository/AreaMasterRepository.java
@@ -1,4 +1,9 @@
 package af.gov.anar.ebreshna.common.network.repository;
 
-public interface AreaMasterRepository {
+import af.gov.anar.ebreshna.common.network.model.AreaMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AreaMasterRepository extends JpaRepository<AreaMaster, Long> {
 }
