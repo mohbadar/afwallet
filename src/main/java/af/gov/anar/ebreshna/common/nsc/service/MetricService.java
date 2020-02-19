b56b9bec29fc914a1bcf92a6524486a1b3333c0b
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 10:43:40 +0430

    metric master service

--- src/main/java/af/gov/anar/ebreshna/common/nsc/service/MetricService.java
+++ src/main/java/af/gov/anar/ebreshna/common/nsc/service/MetricService.java
@@ -1,4 +1,46 @@
 package af.gov.anar.ebreshna.common.nsc.service;
 
+import af.gov.anar.ebreshna.common.nsc.model.MetricMaster;
+import af.gov.anar.ebreshna.common.nsc.repository.MetricRepository;
+import af.gov.anar.ebreshna.common.office.model.DesignationMaster;
+import af.gov.anar.ebreshna.common.office.repository.DesignationMasterRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class MetricService {
+
+    @Autowired
+    private MetricRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public MetricMaster save(MetricMaster obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<MetricMaster> findall()
+    {
+        return repository.findAll();
+    }
+
+    public MetricMaster findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        MetricMaster obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
 }
