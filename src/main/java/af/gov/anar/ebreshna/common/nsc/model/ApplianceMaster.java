d4d875ba3295ce4b5d3b78f9221480f3bdbed10b
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 09:30:21 +0430

    appliance master model

--- src/main/java/af/gov/anar/ebreshna/common/nsc/model/ApplianceMaster.java
+++ src/main/java/af/gov/anar/ebreshna/common/nsc/model/ApplianceMaster.java
@@ -1,4 +1,26 @@
 package af.gov.anar.ebreshna.common.nsc.model;
 
-public class ApplianceMaster {
+
+import af.gov.anar.ebreshna.common.base.BaseEntity;
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "nsc_applaince_master")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+@Audited
+public class ApplianceMaster extends BaseEntity {
+
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
 }
