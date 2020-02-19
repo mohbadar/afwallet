a8d7edf40448eefee814207bddd8b2e5ac756c6d
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 10:33:29 +0430

    repo supplyvoltage master

--- src/main/java/af/gov/anar/ebreshna/common/nsc/repository/SupplyVoltageRepository.java
+++ src/main/java/af/gov/anar/ebreshna/common/nsc/repository/SupplyVoltageRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.common.nsc.repository;
 
-public interface SupplyVoltageRepository {
+import af.gov.anar.ebreshna.common.nsc.model.ActivityMaster;
+import af.gov.anar.ebreshna.common.nsc.model.SupplyVoltage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplyVoltageRepository extends JpaRepository<SupplyVoltage, Long> {
 }
