a4e81901508b2cc57d31a2f8e498e73df50183fe
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 15:28:13 +0430

    complaint

--- src/main/java/af/gov/anar/ebreshna/customerservice/model/Complaint.java
+++ src/main/java/af/gov/anar/ebreshna/customerservice/model/Complaint.java
@@ -2,11 +2,16 @@ package af.gov.anar.ebreshna.customerservice.model;
 
 
 import af.gov.anar.ebreshna.common.base.BaseEntity;
+import af.gov.anar.ebreshna.common.base.province.Province;
+import af.gov.anar.ebreshna.common.csc.model.Request;
+import af.gov.anar.ebreshna.common.csc.model.RequestType;
+import af.gov.anar.ebreshna.common.network.model.DtrCategory;
 import lombok.*;
 import org.hibernate.envers.Audited;
+import org.hibernate.envers.RelationTargetAuditMode;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "customerservice_complaint")
@@ -19,4 +24,51 @@ import javax.persistence.Table;
 @ToString
 @Audited
 public class Complaint extends BaseEntity {
+
+    @ManyToOne(targetEntity = RequestType.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "request_type_id", nullable = false)
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private RequestType requestType;
+
+
+    @ManyToOne(targetEntity = Request.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "request_id", nullable = false)
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private Request request;
+
+    @Column
+    private Date registeredOn;
+
+    @Column
+    private String registeredBy;
+
+    @Column
+    private String requestDetails;
+
+    @Column
+    private String complainantName;
+
+    @Column
+    private  String email;
+
+    @Column
+    private String phoneNo;
+
+    @ManyToOne(targetEntity = Province.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "province_id", nullable = false)
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private Province province;
+
+
+    @Column
+    private String tazkiraDocument;
+    @Column
+    private String identityProofDocument;
+
+    @Column
+    private String customerAccountBookDocument;
+
+
+
+
 }
