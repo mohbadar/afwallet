d95a3359ec5745f724f368d468195549fd617ce6
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 10:30:07 +0430

    repo activity

--- src/main/java/af/gov/anar/ebreshna/common/nsc/repository/ActivityRepository.java
+++ src/main/java/af/gov/anar/ebreshna/common/nsc/repository/ActivityRepository.java
@@ -1,4 +1,9 @@
 package af.gov.anar.ebreshna.common.nsc.repository;
 
-public interface ActivityRepository {
+import af.gov.anar.ebreshna.common.nsc.model.ActivityMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ActivityRepository extends JpaRepository<ActivityMaster, Long> {
 }
