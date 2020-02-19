6af0737e35c66445de545dcc3684c7784ef53a0a
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 10:03:09 +0430

    document category model

--- src/main/java/af/gov/anar/ebreshna/common/nsc/model/DocumentCategoryMaster.java
+++ src/main/java/af/gov/anar/ebreshna/common/nsc/model/DocumentCategoryMaster.java
@@ -1,11 +1,12 @@
 package af.gov.anar.ebreshna.common.nsc.model;
 
 import af.gov.anar.ebreshna.common.base.BaseEntity;
+import af.gov.anar.ebreshna.common.network.model.DtrCategory;
 import lombok.*;
 import org.hibernate.envers.Audited;
+import org.hibernate.envers.RelationTargetAuditMode;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "nsc_document_category_master")
@@ -18,4 +19,18 @@ import javax.persistence.Table;
 @ToString
 @Audited
 public class DocumentCategoryMaster extends BaseEntity {
+
+    @ManyToOne(targetEntity = DtrCategory.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "dtr_category_id", nullable = false)
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private DtrCategory dtrCategory;
+
+
+    @ManyToOne(targetEntity = DocumentMaster.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "document_id", nullable = false)
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private DocumentMaster documentMaster;
+
+    @Column(nullable = false)
+    private int numberOfCopies;
 }
