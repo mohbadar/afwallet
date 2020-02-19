57f50989d6a240723e054edc3d5a219c98d55663
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 15:40:15 +0430

    workflow tranistion data controller

--- src/main/java/af/gov/anar/ebreshna/common/base/workflowdata/WorkflowTransitionDataController.java
+++ src/main/java/af/gov/anar/ebreshna/common/base/workflowdata/WorkflowTransitionDataController.java
@@ -1,4 +1,48 @@
 package af.gov.anar.ebreshna.common.base.workflowdata;
 
-public class WorkflowTransitionDataController {
+import af.gov.anar.api.handler.ResponseHandler;
+import af.gov.anar.ebreshna.helpdesk.model.Comment;
+import af.gov.anar.ebreshna.helpdesk.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/common/workflowtransitiondata")
+public class WorkflowTransitionDataController extends ResponseHandler {
+
+    @Autowired
+    private WorkflowTransitionDataService service;
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<WorkflowTransitionData>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<WorkflowTransitionData> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<WorkflowTransitionData> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) WorkflowTransitionData obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<WorkflowTransitionData> save(@RequestBody(required = true) WorkflowTransitionData obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
