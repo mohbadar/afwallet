7b10fbdf6cd1fdfe454c52e38d43f717a827ea0c
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 10:31:46 +0430

    repo document

--- src/main/java/af/gov/anar/ebreshna/common/nsc/repository/DocumentRepository.java
+++ src/main/java/af/gov/anar/ebreshna/common/nsc/repository/DocumentRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.common.nsc.repository;
 
-public interface DocumentRepository {
+import af.gov.anar.ebreshna.common.nsc.model.ActivityMaster;
+import af.gov.anar.ebreshna.common.nsc.model.DocumentMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DocumentRepository extends JpaRepository<DocumentMaster, Long> {
 }
