0c3b2ee29a658d2c59bebd8183c2e06009e4fd46
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 10:31:04 +0430

    repo applaince

--- src/main/java/af/gov/anar/ebreshna/common/nsc/repository/ApplainceRepository.java
+++ src/main/java/af/gov/anar/ebreshna/common/nsc/repository/ApplainceRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.common.nsc.repository;
 
-public interface ApplainceRepository {
+import af.gov.anar.ebreshna.common.nsc.model.ActivityMaster;
+import af.gov.anar.ebreshna.common.nsc.model.ApplianceMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApplainceRepository extends JpaRepository<ApplianceMaster, Long> {
 }
