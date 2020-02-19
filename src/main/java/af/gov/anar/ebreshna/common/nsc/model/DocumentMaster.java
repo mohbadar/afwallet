047048df073cfe4639db299dc1a26423d7e8ea94
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 10:01:31 +0430

    document master model

--- src/main/java/af/gov/anar/ebreshna/common/nsc/model/DocumentMaster.java
+++ src/main/java/af/gov/anar/ebreshna/common/nsc/model/DocumentMaster.java
@@ -4,6 +4,7 @@ import af.gov.anar.ebreshna.common.base.BaseEntity;
 import lombok.*;
 import org.hibernate.envers.Audited;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -18,4 +19,7 @@ import javax.persistence.Table;
 @ToString
 @Audited
 public class DocumentMaster extends BaseEntity {
+
+    @Column(nullable = false)
+    private String name;
 }
