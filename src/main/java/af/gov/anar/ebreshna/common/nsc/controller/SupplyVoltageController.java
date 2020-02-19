74c866441fcb27d904305c342f31b573f93af3f9
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 10:51:00 +0430

    nsc controller endpoints

--- src/main/java/af/gov/anar/ebreshna/common/nsc/controller/SupplyVoltageController.java
+++ src/main/java/af/gov/anar/ebreshna/common/nsc/controller/SupplyVoltageController.java
@@ -1,4 +1,9 @@
 package af.gov.anar.ebreshna.common.nsc.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/config/nsc/supplyvoltages")
 public class SupplyVoltageController {
 }
