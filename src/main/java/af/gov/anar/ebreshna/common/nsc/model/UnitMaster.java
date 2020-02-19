40c95c149d6bda79db821ac8fae0488c9bffe99f
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 09:33:10 +0430

    unit master model

--- src/main/java/af/gov/anar/ebreshna/common/nsc/model/UnitMaster.java
+++ src/main/java/af/gov/anar/ebreshna/common/nsc/model/UnitMaster.java
@@ -1,4 +1,27 @@
 package af.gov.anar.ebreshna.common.nsc.model;
 
-public class UnitMaster {
+import af.gov.anar.ebreshna.common.base.BaseEntity;
+import af.gov.anar.ebreshna.helpdesk.model.Complaint;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "nsc_unit_master")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+@Audited
+public class UnitMaster extends BaseEntity {
+
+    @ManyToOne(targetEntity = ApplianceMaster.class, fetch = FetchType.EAGER)
+    @JoinColumn(name = "appliance_id", nullable = false)
+    @JsonIgnore
+    private ApplianceMaster applianceMaster;
 }
