0e78f636a95fd66575b1dc107b9c4d58be93420a
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 10:34:12 +0430

    repo unit type master

--- src/main/java/af/gov/anar/ebreshna/common/nsc/repository/UnitTypeRepository.java
+++ src/main/java/af/gov/anar/ebreshna/common/nsc/repository/UnitTypeRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.common.nsc.repository;
 
-public interface UnitTypeRepository {
+import af.gov.anar.ebreshna.common.nsc.model.ActivityMaster;
+import af.gov.anar.ebreshna.common.nsc.model.UnitType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UnitTypeRepository extends JpaRepository<UnitType, Long> {
 }
