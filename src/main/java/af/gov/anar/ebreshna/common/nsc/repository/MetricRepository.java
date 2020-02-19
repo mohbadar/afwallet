8eeddff6450941edffff3612cfae241fd57c57cd
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 10:32:50 +0430

    repo metic master

--- src/main/java/af/gov/anar/ebreshna/common/nsc/repository/MetricRepository.java
+++ src/main/java/af/gov/anar/ebreshna/common/nsc/repository/MetricRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.common.nsc.repository;
 
-public interface MetricRepository {
+import af.gov.anar.ebreshna.common.nsc.model.ActivityMaster;
+import af.gov.anar.ebreshna.common.nsc.model.MetricMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MetricRepository extends JpaRepository<MetricMaster, Long> {
 }
