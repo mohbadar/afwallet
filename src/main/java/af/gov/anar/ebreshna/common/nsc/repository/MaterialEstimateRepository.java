bc5bbab3d734379e29a0e1ead78e5fbdb1be79a7
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-19 10:32:11 +0430

    repo material estimate

--- src/main/java/af/gov/anar/ebreshna/common/nsc/repository/MaterialEstimateRepository.java
+++ src/main/java/af/gov/anar/ebreshna/common/nsc/repository/MaterialEstimateRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.common.nsc.repository;
 
-public interface MaterialEstimateRepository {
+import af.gov.anar.ebreshna.common.nsc.model.ActivityMaster;
+import af.gov.anar.ebreshna.common.nsc.model.MaterialEstimateMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MaterialEstimateRepository extends JpaRepository<MaterialEstimateMaster, Long> {
 }
