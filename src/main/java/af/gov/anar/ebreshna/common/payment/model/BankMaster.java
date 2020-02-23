53d398ce2ea1a36ff923c7e8f24e45919e0c5f5a
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-23 14:00:01 +0430

    print type

--- src/main/java/af/gov/anar/ebreshna/common/payment/model/BankMaster.java
+++ src/main/java/af/gov/anar/ebreshna/common/payment/model/BankMaster.java
@@ -5,6 +5,7 @@ import af.gov.anar.ebreshna.infrastructure.util.Schema;
 import lombok.*;
 import org.hibernate.envers.Audited;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -19,4 +20,23 @@ import javax.persistence.Table;
 @ToString
 @Audited
 public class BankMaster extends BaseEntity {
+
+
+    @Column
+    private String shortName;
+
+    @Column
+    private String bankName;
+
+    @Column
+    private String bankCode;
+
+    @Column
+    private String logo;
+
+    @Column
+    private boolean makerAndCheckerRequired;
+
+    @Column
+    private String printType;
 }
