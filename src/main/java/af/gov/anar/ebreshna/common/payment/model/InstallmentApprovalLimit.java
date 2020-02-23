e7632cbeeec269cb1392516382f60273901132ef
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-23 13:56:18 +0430

    payment model initial config structure

--- src/main/java/af/gov/anar/ebreshna/common/payment/model/InstallmentApprovalLimit.java
+++ src/main/java/af/gov/anar/ebreshna/common/payment/model/InstallmentApprovalLimit.java
@@ -1,4 +1,22 @@
 package af.gov.anar.ebreshna.common.payment.model;
 
-public class InstallmentApprovalLimit {
+import af.gov.anar.ebreshna.common.base.BaseEntity;
+import af.gov.anar.ebreshna.infrastructure.util.Schema;
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "payment_installment_approval_limit", schema = Schema.CORE_SCHEMA)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+@Audited
+public class InstallmentApprovalLimit extends BaseEntity {
 }
--- src/main/java/af/gov/anar/ebreshna/common/payment/model/PostingDaysConfig.java
+++ src/main/java/af/gov/anar/ebreshna/common/payment/model/PostingDaysConfig.java
@@ -1,4 +1,22 @@
 package af.gov.anar.ebreshna.common.payment.model;
 
-public class PostingDaysConfig {
+import af.gov.anar.ebreshna.common.base.BaseEntity;
+import af.gov.anar.ebreshna.infrastructure.util.Schema;
+import lombok.*;
+import org.hibernate.envers.Audited;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "payment_posting_days_config", schema = Schema.CORE_SCHEMA)
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+@Audited
+public class PostingDaysConfig extends BaseEntity {
 }
