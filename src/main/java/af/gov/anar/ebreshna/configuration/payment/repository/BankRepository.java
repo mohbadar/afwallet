6afa2df5153bf984263a4de543b8cd2505849910
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-24 12:05:11 +0430

    #1 bank master repo

--- src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/BankRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/BankRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.payment.repository;
 
-public interface BankRepository {
+import af.gov.anar.ebreshna.configuration.payment.model.BankBranch;
+import af.gov.anar.ebreshna.configuration.payment.model.BankMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BankRepository extends JpaRepository<BankMaster, Long> {
 }
