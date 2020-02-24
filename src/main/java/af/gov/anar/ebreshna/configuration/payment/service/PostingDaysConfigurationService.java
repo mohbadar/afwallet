91815f5456ce1d0a8883a2a39fc7345ee78adb56
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-24 13:02:16 +0430

    #1 service: posting days config

--- src/main/java/af/gov/anar/ebreshna/configuration/payment/service/PostingDaysConfigurationService.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/payment/service/PostingDaysConfigurationService.java
@@ -1,4 +1,47 @@
 package af.gov.anar.ebreshna.configuration.payment.service;
 
+import af.gov.anar.ebreshna.configuration.payment.model.CounterMaster;
+import af.gov.anar.ebreshna.configuration.payment.model.PostingDaysConfiguration;
+import af.gov.anar.ebreshna.configuration.payment.repository.CounterRepository;
+import af.gov.anar.ebreshna.configuration.payment.repository.PostingDaysConfigurationRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class PostingDaysConfigurationService {
+
+    @Autowired
+    private PostingDaysConfigurationRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public PostingDaysConfiguration save(PostingDaysConfiguration obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<PostingDaysConfiguration> findall()
+    {
+        return repository.findAll();
+    }
+
+    public PostingDaysConfiguration findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        PostingDaysConfiguration obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
+
 }
