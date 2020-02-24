c35906da8f173f05e52c9e4910a4b53945ca4a6a
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-24 12:10:05 +0430

    #1 repository: installment approval limit

--- src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/InstallmentApprovalLimitRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/InstallmentApprovalLimitRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.payment.repository;
 
-public interface InstallmentApprovalLimitRepository {
+import af.gov.anar.ebreshna.configuration.payment.model.BankBranch;
+import af.gov.anar.ebreshna.configuration.payment.model.InstallmentApprovalLimit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InstallmentApprovalLimitRepository extends JpaRepository<InstallmentApprovalLimit, Long> {
 }
