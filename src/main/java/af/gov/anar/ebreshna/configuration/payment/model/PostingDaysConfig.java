ff4ccaa3c15d6420f532bf5089d1903e4a61a1a5
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-24 11:35:09 +0430

    posting days config model'

--- src/main/java/af/gov/anar/ebreshna/configuration/payment/model/PostingDaysConfig.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/payment/model/PostingDaysConfig.java
@@ -1,12 +1,13 @@
 package af.gov.anar.ebreshna.configuration.payment.model;
 
 import af.gov.anar.ebreshna.configuration.common.BaseEntity;
+import af.gov.anar.ebreshna.configuration.common.tariff.model.TariffCategory;
 import af.gov.anar.ebreshna.infrastructure.util.Schema;
 import lombok.*;
 import org.hibernate.envers.Audited;
+import org.hibernate.envers.RelationTargetAuditMode;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "payment_posting_days_config", schema = Schema.CORE_SCHEMA)
@@ -19,4 +20,19 @@ import javax.persistence.Table;
 @ToString
 @Audited
 public class PostingDaysConfig extends BaseEntity {
+
+
+    @OneToOne(targetEntity = BankBranch.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = true, name = "bank_branch_id")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private BankBranch bankBranch;
+
+
+    @OneToOne(targetEntity = BankMaster.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = true, name = "bank_master_id")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private BankMaster bankMaster;
+
+    @Column
+    private int daysConfigured;
 }
