5d0d08f7467c19c07edbd671e5806f5921515eb1
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-24 13:20:52 +0430

    #1 controller: installment penalty limit

--- src/main/java/af/gov/anar/ebreshna/configuration/payment/controller/InstallmentApprovalLimitController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/payment/controller/InstallmentApprovalLimitController.java
@@ -1,9 +1,56 @@
 package af.gov.anar.ebreshna.configuration.payment.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.api.handler.ResponseHandler;
+import af.gov.anar.ebreshna.configuration.payment.model.BankBranch;
+import af.gov.anar.ebreshna.configuration.payment.model.InstallmentApprovalLimit;
+import af.gov.anar.ebreshna.configuration.payment.service.BankBranchService;
+import af.gov.anar.ebreshna.configuration.payment.service.InstallmentApprovalLimitService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/config/payment/installment-approval-limits")
-public class InstallmentApprovalLimitController {
+public class InstallmentApprovalLimitController extends ResponseHandler {
+
+    @Autowired
+    private InstallmentApprovalLimitService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<InstallmentApprovalLimit>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<InstallmentApprovalLimit> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<InstallmentApprovalLimit> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) InstallmentApprovalLimit obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<InstallmentApprovalLimit> save(@RequestBody(required = true) InstallmentApprovalLimit obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
