2eb94c72a3f0fedeb360d886d2de432dcf4d69a9
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-24 12:05:48 +0430

    #1 repository: counter

--- src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/CounterRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/CounterRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.payment.repository;
 
-public interface CounterRepository {
+import af.gov.anar.ebreshna.configuration.payment.model.BankBranch;
+import af.gov.anar.ebreshna.configuration.payment.model.CounterMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CounterRepository extends JpaRepository<CounterMaster, Long> {
 }
