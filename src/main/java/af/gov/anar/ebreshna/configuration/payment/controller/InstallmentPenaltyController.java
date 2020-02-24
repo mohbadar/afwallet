764e400500baaea9f1e65ec01b240f7684b01ac9
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-24 13:21:42 +0430

    #1 controller: installment penalty

--- src/main/java/af/gov/anar/ebreshna/configuration/payment/controller/InstallmentPenaltyController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/payment/controller/InstallmentPenaltyController.java
@@ -1,9 +1,55 @@
 package af.gov.anar.ebreshna.configuration.payment.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.api.handler.ResponseHandler;
+import af.gov.anar.ebreshna.configuration.payment.model.BankBranch;
+import af.gov.anar.ebreshna.configuration.payment.model.InstallmentPenalty;
+import af.gov.anar.ebreshna.configuration.payment.service.BankBranchService;
+import af.gov.anar.ebreshna.configuration.payment.service.InstallmentPenaltyService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/config/payment/installment-penalties")
-public class InstallmentPenaltyController {
+public class InstallmentPenaltyController extends ResponseHandler {
+    @Autowired
+    private InstallmentPenaltyService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<InstallmentPenalty>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<InstallmentPenalty> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<InstallmentPenalty> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) InstallmentPenalty obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<InstallmentPenalty> save(@RequestBody(required = true) InstallmentPenalty obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
