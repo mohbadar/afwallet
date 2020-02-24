a99642c2753ca4923d140e259c8e55fb03e0d74e
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-24 12:11:59 +0430

    #1 repository: posting days configuration

--- src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/PostingDaysConfigurationRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/PostingDaysConfigurationRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.payment.repository;
 
-public interface PostingDaysConfigurationRepository {
+import af.gov.anar.ebreshna.configuration.payment.model.BankBranch;
+import af.gov.anar.ebreshna.configuration.payment.model.PostingDaysConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostingDaysConfigurationRepository extends JpaRepository<PostingDaysConfiguration, Long> {
 }
