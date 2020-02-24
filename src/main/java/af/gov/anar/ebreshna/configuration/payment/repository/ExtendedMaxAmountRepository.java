3d1c65b5e9bb9b1a17ad4b311961fcec3b248bfb
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-24 12:06:14 +0430

    #1 repository: extended max amount

--- src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/ExtendedMaxAmountRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/ExtendedMaxAmountRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.payment.repository;
 
-public interface ExtendedMaxAmountRepository {
+import af.gov.anar.ebreshna.configuration.payment.model.BankBranch;
+import af.gov.anar.ebreshna.configuration.payment.model.ExtendedMaxAmount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExtendedMaxAmountRepository extends JpaRepository<ExtendedMaxAmount, Long> {
 }
