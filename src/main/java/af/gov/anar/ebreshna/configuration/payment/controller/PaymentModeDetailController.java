71f9e848f5a9ad753dde4f8156c1cab76fb4265d
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-24 13:14:04 +0430

    #1 controller: endpoints

--- src/main/java/af/gov/anar/ebreshna/configuration/payment/controller/PaymentModeDetailController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/payment/controller/PaymentModeDetailController.java
@@ -1,4 +1,9 @@
 package af.gov.anar.ebreshna.configuration.payment.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/config/payment/payment-modes")
 public class PaymentModeDetailController {
 }
