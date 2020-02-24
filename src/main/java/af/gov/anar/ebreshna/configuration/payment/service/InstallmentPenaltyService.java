12510a04b478d85aa4719cc8f6f1d5d43e607fe9
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-24 13:00:16 +0430

    #1 service: installment penalty

--- src/main/java/af/gov/anar/ebreshna/configuration/payment/service/InstallmentPenaltyService.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/payment/service/InstallmentPenaltyService.java
@@ -1,4 +1,46 @@
 package af.gov.anar.ebreshna.configuration.payment.service;
 
+import af.gov.anar.ebreshna.configuration.payment.model.CounterMaster;
+import af.gov.anar.ebreshna.configuration.payment.model.InstallmentPanelty;
+import af.gov.anar.ebreshna.configuration.payment.repository.CounterRepository;
+import af.gov.anar.ebreshna.configuration.payment.repository.InstallmentPenalyRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class InstallmentPenaltyService {
+
+    @Autowired
+    private InstallmentPenalyRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public InstallmentPanelty save(InstallmentPanelty obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<InstallmentPanelty> findall()
+    {
+        return repository.findAll();
+    }
+
+    public InstallmentPanelty findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        InstallmentPanelty obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
 }
