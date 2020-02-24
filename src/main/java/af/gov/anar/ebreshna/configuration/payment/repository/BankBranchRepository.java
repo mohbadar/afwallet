3c56483a3015db9371cd2c1092088e404ec36101
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-24 12:04:37 +0430

    #1 bank branch repo

--- src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/BankBranchRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/BankBranchRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.payment.repository;
 
-public interface BankBranchRepository {
+import af.gov.anar.ebreshna.configuration.payment.model.BankBranch;
+import org.springframework.boot.actuate.audit.AuditEventRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BankBranchRepository extends JpaRepository<BankBranch, Long> {
 }
