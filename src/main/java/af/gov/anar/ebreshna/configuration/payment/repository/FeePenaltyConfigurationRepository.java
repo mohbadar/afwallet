9a8fc3bb83f9b39373976e39c391305bd046b07c
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-24 12:06:57 +0430

    #1 repository: fee penalty config

--- src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/FeePenaltyConfigurationRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/payment/repository/FeePenaltyConfigurationRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.payment.repository;
 
-public interface FeePenaltyConfigurationRepository {
+import af.gov.anar.ebreshna.configuration.payment.model.BankBranch;
+import af.gov.anar.ebreshna.configuration.payment.model.FeePenaltyConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeePenaltyConfigurationRepository extends JpaRepository<FeePenaltyConfiguration, Long> {
 }
