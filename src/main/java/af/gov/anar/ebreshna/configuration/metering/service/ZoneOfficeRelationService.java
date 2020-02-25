282501510513cc40c5b2646dce9c095f947af809
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 17:01:08 +0430

    #2 metering : zone tariff relation service

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/service/ZoneOfficeRelationService.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/service/ZoneOfficeRelationService.java
@@ -1,4 +1,46 @@
 package af.gov.anar.ebreshna.configuration.metering.service;
 
+import af.gov.anar.ebreshna.configuration.metering.model.MeterMakeSerial;
+import af.gov.anar.ebreshna.configuration.metering.repository.MeterMakeSerialRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class ZoneOfficeRelationService {
+
+    @Autowired
+    private MeterMakeSerialRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public MeterMakeSerial save(MeterMakeSerial obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<MeterMakeSerial> findall()
+    {
+        return repository.findAll();
+    }
+
+    public MeterMakeSerial findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        MeterMakeSerial obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
+
+
 }
--- src/main/java/af/gov/anar/ebreshna/configuration/metering/service/ZoneTariffRelationService.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/service/ZoneTariffRelationService.java
@@ -1,4 +1,46 @@
 package af.gov.anar.ebreshna.configuration.metering.service;
 
+import af.gov.anar.ebreshna.configuration.metering.model.Zone;
+import af.gov.anar.ebreshna.configuration.metering.model.ZoneTariffRelation;
+import af.gov.anar.ebreshna.configuration.metering.repository.ZoneRepository;
+import af.gov.anar.ebreshna.configuration.metering.repository.ZoneTariffRelationRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class ZoneTariffRelationService {
+
+    @Autowired
+    private ZoneTariffRelationRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public ZoneTariffRelation save(ZoneTariffRelation obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<ZoneTariffRelation> findall()
+    {
+        return repository.findAll();
+    }
+
+    public ZoneTariffRelation findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        ZoneTariffRelation obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
 }
