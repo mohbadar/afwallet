9ed49cf7dcb10c28cef88211dd1f735a510d2947
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 11:42:48 +0430

    #2 metering: zone office relation

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/model/ZoneOfficeRelation.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/model/ZoneOfficeRelation.java
@@ -1,11 +1,13 @@
 package af.gov.anar.ebreshna.configuration.metering.model;
 
 import af.gov.anar.ebreshna.configuration.common.BaseEntity;
+import af.gov.anar.ebreshna.configuration.common.province.Province;
+import af.gov.anar.ebreshna.configuration.office.model.OfficeMaster;
 import lombok.*;
 import org.hibernate.envers.Audited;
+import org.hibernate.envers.RelationTargetAuditMode;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "metering_zone_office_relation")
@@ -18,4 +20,19 @@ import javax.persistence.Table;
 @ToString
 @Audited
 public class ZoneOfficeRelation extends BaseEntity {
+
+    @OneToOne(targetEntity = Province.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "province_id")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private Province province;
+
+    @OneToOne(targetEntity = Zone.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "zone_id")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private Zone zone;
+
+    @OneToOne(targetEntity = OfficeMaster.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "office_master_id")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private OfficeMaster officeMaster;
 }
