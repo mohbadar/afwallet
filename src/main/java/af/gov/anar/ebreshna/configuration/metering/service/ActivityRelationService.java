fce037a0ab434610575671edd980fdb70af04ee4
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 16:03:05 +0430

    activity relation service

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/service/ActivityRelationService.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/service/ActivityRelationService.java
@@ -1,4 +1,46 @@
 package af.gov.anar.ebreshna.configuration.metering.service;
 
+import af.gov.anar.ebreshna.configuration.metering.model.ActivityRelation;
+import af.gov.anar.ebreshna.configuration.metering.repository.ActivityRelationRepository;
+import af.gov.anar.ebreshna.configuration.network.model.AreaMaster;
+import af.gov.anar.ebreshna.configuration.network.repository.AreaMasterRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class ActivityRelationService {
+
+    @Autowired
+    private ActivityRelationRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public ActivityRelation save(ActivityRelation obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<ActivityRelation> findall()
+    {
+        return repository.findAll();
+    }
+
+    public ActivityRelation findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        ActivityRelation obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
 }
