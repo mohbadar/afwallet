efe66f20e112fff0b5c7d1805b82e1cce1b7cc6b
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 11:46:19 +0430

    #2 metering:  office cycle relation

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/model/OfficeCycleRelation.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/model/OfficeCycleRelation.java
@@ -1,11 +1,13 @@
 package af.gov.anar.ebreshna.configuration.metering.model;
 
 import af.gov.anar.ebreshna.configuration.common.BaseEntity;
+import af.gov.anar.ebreshna.configuration.common.province.Province;
+import af.gov.anar.ebreshna.configuration.office.model.OfficeMaster;
 import lombok.*;
 import org.hibernate.envers.Audited;
+import org.hibernate.envers.RelationTargetAuditMode;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "metering_office_cycle_relation")
@@ -18,4 +20,19 @@ import javax.persistence.Table;
 @ToString
 @Audited
 public class OfficeCycleRelation extends BaseEntity {
+
+    @OneToOne(targetEntity = Province.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "province_id")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private Province province;
+
+    @OneToOne(targetEntity = CycleConfiguration.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "cycle_configuration_id")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private CycleConfiguration cycleConfiguration;
+
+    @OneToOne(targetEntity = OfficeMaster.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "office_master_id")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private OfficeMaster officeMaster;
 }
