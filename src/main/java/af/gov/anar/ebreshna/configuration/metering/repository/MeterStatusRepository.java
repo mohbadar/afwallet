dbfeb733f52d6d467630a2abd1d9d9587a0bdc0a
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 15:42:27 +0430

    #2 metering: meter status master

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/MeterStatusRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/MeterStatusRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.metering.repository;
 
-public interface MeterStatusRepository {
+import af.gov.anar.ebreshna.configuration.metering.model.ActivityRelation;
+import af.gov.anar.ebreshna.configuration.metering.model.MeterStatusMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeterStatusRepository extends JpaRepository<MeterStatusMaster, Long> {
 }
