48cce61f07bf96641bb0aafd0221bbfb34f86804
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 15:47:40 +0430

    #2 zone tariff relation model

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/ZoneTariffRelationRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/ZoneTariffRelationRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.metering.repository;
 
-public interface ZoneTariffRelationRepository {
+import af.gov.anar.ebreshna.configuration.metering.model.ActivityRelation;
+import af.gov.anar.ebreshna.configuration.metering.model.ZoneTariffRelation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ZoneTariffRelationRepository extends JpaRepository<ZoneTariffRelation, Long> {
 }
