ff3dff20a69377cc086a3bb9eedebb19e7da8362
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 15:42:04 +0430

    #2 metering: meter make serial

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/MeterMakeSerialRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/MeterMakeSerialRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.metering.repository;
 
-public interface MeterMakeSerialRepository {
+import af.gov.anar.ebreshna.configuration.metering.model.ActivityRelation;
+import af.gov.anar.ebreshna.configuration.metering.model.MeterMakeSerial;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeterMakeSerialRepository extends JpaRepository<MeterMakeSerial, Long> {
 }
