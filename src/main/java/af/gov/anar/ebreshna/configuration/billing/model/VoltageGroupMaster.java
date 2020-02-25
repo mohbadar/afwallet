3b80ceb5aa1d331a2ff9a419f7469da5f240002d
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 10:49:17 +0430

    #3 billing: voltage group

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/model/VoltageGroupMaster.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/model/VoltageGroupMaster.java
@@ -1,11 +1,11 @@
 package af.gov.anar.ebreshna.configuration.billing.model;
 
+import af.gov.anar.ebreshna.configuration.billing.enumeration.VoltageGroup;
 import af.gov.anar.ebreshna.configuration.common.BaseEntity;
 import lombok.*;
 import org.hibernate.envers.Audited;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "billing_voltage_group_master")
@@ -18,4 +18,24 @@ import javax.persistence.Table;
 @ToString
 @Audited
 public class VoltageGroupMaster extends BaseEntity {
+
+    @ManyToOne(targetEntity = VoltageUnit.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "voltage_unit_id")
+    private VoltageUnit voltageUnit;
+
+    @Column
+    private String voltageValue;
+
+    @Column
+    private VoltageGroup voltageGroup;
+
+//    @ManyToOne(targetEntity = Phase.class, fetch = FetchType.EAGER)
+//    @JoinColumn(nullable = false, name = "tariff_charge_id")
+//    private TariffCharge tariffCharge;
+
+    @Column
+    private String phase;
+
+    @Column
+    private String voltageDescription;
 }
