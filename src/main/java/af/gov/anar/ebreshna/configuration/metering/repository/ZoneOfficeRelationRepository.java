796ba929b3f49ed2f4f08345427e20449efb7289
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 15:46:45 +0430

    #2 zone office relation

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/ZoneOfficeRelationRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/ZoneOfficeRelationRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.metering.repository;
 
-public interface ZoneOfficeRelationRepository {
+import af.gov.anar.ebreshna.configuration.metering.model.ActivityRelation;
+import af.gov.anar.ebreshna.configuration.metering.model.ZoneOfficeRelation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ZoneOfficeRelationRepository extends JpaRepository<ZoneOfficeRelation, Long> {
 }
