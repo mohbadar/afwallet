8937667b82354fc921a6e6a7429e557432e2de05
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 15:38:50 +0430

    #2 metering: meter capacity

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/CycleRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/CycleRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.metering.repository;
 
-public interface CycleRepository {
+import af.gov.anar.ebreshna.configuration.metering.model.ActivityRelation;
+import af.gov.anar.ebreshna.configuration.metering.model.CycleConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CycleRepository extends JpaRepository<CycleConfiguration, Long> {
 }
--- src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/MeterCapacityRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/MeterCapacityRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.metering.repository;
 
-public interface MeterCapacityRepository {
+import af.gov.anar.ebreshna.configuration.metering.model.ActivityRelation;
+import af.gov.anar.ebreshna.configuration.metering.model.MeterCapacityMaster;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeterCapacityRepository extends JpaRepository<MeterCapacityMaster, Long> {
 }
