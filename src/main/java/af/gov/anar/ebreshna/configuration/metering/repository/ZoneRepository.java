950d5e3ff4bb656ef50d0a6616d8f9b0f3e7ab46
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 15:47:06 +0430

    #2 zone model

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/ZoneRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/ZoneRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.metering.repository;
 
-public interface ZoneRepository {
+import af.gov.anar.ebreshna.configuration.metering.model.ActivityRelation;
+import af.gov.anar.ebreshna.configuration.metering.model.Zone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ZoneRepository extends JpaRepository<Zone, Long> {
 }
