93c700ed560dbde59bb5920e8241b59c43b5b606
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 15:40:56 +0430

    #2 metering: metering type

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/MeteringTypeRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/MeteringTypeRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.metering.repository;
 
-public interface MeteringTypeRepository {
+import af.gov.anar.ebreshna.configuration.metering.model.ActivityRelation;
+import af.gov.anar.ebreshna.configuration.metering.model.MeteringType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MeteringTypeRepository extends JpaRepository<MeteringType, Long> {
 }
