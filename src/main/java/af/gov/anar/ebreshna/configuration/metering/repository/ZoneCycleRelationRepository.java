5dcb6f17c58f39c8c2797fa711fa04e1ce572118
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 15:43:55 +0430

    #2 zone cycle relation

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/ZoneCycleRelationRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/ZoneCycleRelationRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.metering.repository;
 
-public interface ZoneCycleRelationRepository {
+import af.gov.anar.ebreshna.configuration.metering.model.ActivityRelation;
+import af.gov.anar.ebreshna.configuration.metering.model.ZoneCycleRelation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ZoneCycleRelationRepository extends JpaRepository<ZoneCycleRelation, Long> {
 }
