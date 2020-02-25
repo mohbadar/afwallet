130770f2364bc46a26e63ae26f98dbeeded8bb75
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 11:33:21 +0430

    #2 metering: zone model

---
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/enumeration/BillMode.java
@@ -0,0 +1,7 @@
+package af.gov.anar.ebreshna.configuration.metering.enumeration;
+
+public enum BillMode {
+
+    BI_MONTHLY,
+    MONTHLY;
+}
--- src/main/java/af/gov/anar/ebreshna/configuration/metering/model/Zone.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/model/Zone.java
@@ -1,11 +1,13 @@
 package af.gov.anar.ebreshna.configuration.metering.model;
 
 import af.gov.anar.ebreshna.configuration.common.BaseEntity;
+import af.gov.anar.ebreshna.configuration.common.province.Province;
+import af.gov.anar.ebreshna.configuration.metering.enumeration.BillMode;
 import lombok.*;
 import org.hibernate.envers.Audited;
+import org.hibernate.envers.RelationTargetAuditMode;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "metering_zone")
@@ -18,4 +20,19 @@ import javax.persistence.Table;
 @ToString
 @Audited
 public class Zone extends BaseEntity {
+
+    @OneToOne(targetEntity = Province.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = true, name = "province_id")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private Province province;
+
+    @Column
+    private String name;
+
+    @Column
+    private String code;
+
+
+    @Column
+    private BillMode billMode;
 }
