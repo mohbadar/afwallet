c67fc2a806b3a3cb5b5ae023d9bbb7ce0fee7c70
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 15:43:12 +0430

    #2 office cycle relation

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/OfficeCycleRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/OfficeCycleRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.metering.repository;
 
-public interface OfficeCycleRepository {
+import af.gov.anar.ebreshna.configuration.metering.model.ActivityRelation;
+import af.gov.anar.ebreshna.configuration.metering.model.OfficeCycleRelation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OfficeCycleRepository extends JpaRepository<OfficeCycleRelation, Long> {
 }
