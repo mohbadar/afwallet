bce35e037a6f46ab824ff78023b57b9b38858f55
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 16:07:02 +0430

    #2 metering activity service

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/ActivityRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/repository/ActivityRepository.java
@@ -1,10 +1,9 @@
 package af.gov.anar.ebreshna.configuration.metering.repository;
 
-import af.gov.anar.ebreshna.configuration.metering.model.ActivityMaster;
-import af.gov.anar.ebreshna.configuration.metering.model.ActivityRelation;
+import af.gov.anar.ebreshna.configuration.metering.model.MeteringActivityMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ActivityRepository extends JpaRepository<ActivityMaster, Long> {
+public interface ActivityRepository extends JpaRepository<MeteringActivityMaster, Long> {
 }
--- src/main/java/af/gov/anar/ebreshna/configuration/metering/service/ActivityService.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/service/ActivityService.java
@@ -1,4 +1,46 @@
 package af.gov.anar.ebreshna.configuration.metering.service;
 
+import af.gov.anar.ebreshna.configuration.metering.model.MeteringActivityMaster;
+import af.gov.anar.ebreshna.configuration.metering.repository.ActivityRepository;
+import af.gov.anar.ebreshna.configuration.network.model.AreaMaster;
+import af.gov.anar.ebreshna.configuration.network.repository.AreaMasterRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class ActivityService {
+
+    @Autowired
+    private ActivityRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public MeteringActivityMaster save(MeteringActivityMaster obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<MeteringActivityMaster> findall()
+    {
+        return repository.findAll();
+    }
+
+    public MeteringActivityMaster findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        MeteringActivityMaster obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
 }
