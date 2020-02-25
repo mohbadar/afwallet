d882d5357656d3b98b120c4708f51fd6aad4368c
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 11:43:53 +0430

    #2 metering: zone tariff relation

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/model/ZoneTariffRelation.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/model/ZoneTariffRelation.java
@@ -1,11 +1,14 @@
 package af.gov.anar.ebreshna.configuration.metering.model;
 
 import af.gov.anar.ebreshna.configuration.common.BaseEntity;
+import af.gov.anar.ebreshna.configuration.common.province.Province;
+import af.gov.anar.ebreshna.configuration.common.tariff.model.TariffCategory;
+import af.gov.anar.ebreshna.configuration.office.model.OfficeMaster;
 import lombok.*;
 import org.hibernate.envers.Audited;
+import org.hibernate.envers.RelationTargetAuditMode;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "metering_zone_tariff_relation")
@@ -18,4 +21,19 @@ import javax.persistence.Table;
 @ToString
 @Audited
 public class ZoneTariffRelation extends BaseEntity {
+
+    @OneToOne(targetEntity = Province.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "province_id")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private Province province;
+
+    @OneToOne(targetEntity = Zone.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "zone_id")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private Zone zone;
+
+    @OneToOne(targetEntity = TariffCategory.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "tariff_category_id")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private TariffCategory tariffCategory;
 }
