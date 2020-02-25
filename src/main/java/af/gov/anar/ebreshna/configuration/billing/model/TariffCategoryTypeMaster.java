1525489473c71ff14b33c9ede35f11aa08f0f6be
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-25 10:04:14 +0430

    #3 billing: tariff category type

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/model/TariffCategoryTypeMaster.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/model/TariffCategoryTypeMaster.java
@@ -4,6 +4,7 @@ import af.gov.anar.ebreshna.configuration.common.BaseEntity;
 import lombok.*;
 import org.hibernate.envers.Audited;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -18,4 +19,9 @@ import javax.persistence.Table;
 @ToString
 @Audited
 public class TariffCategoryTypeMaster extends BaseEntity {
+
+    @Column
+    private String name;
+
+
 }
