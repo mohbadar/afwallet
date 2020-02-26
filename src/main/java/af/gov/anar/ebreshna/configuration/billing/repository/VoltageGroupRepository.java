51c3efede87225e2145096bb3869f8c30e1aba27
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 10:51:51 +0430

    #2 billing service: voltage group

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/repository/VoltageGroupRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/repository/VoltageGroupRepository.java
@@ -2,9 +2,10 @@ package af.gov.anar.ebreshna.configuration.billing.repository;
 
 import af.gov.anar.ebreshna.configuration.billing.enumeration.VoltageGroup;
 import af.gov.anar.ebreshna.configuration.billing.model.TrLossesConfiguration;
+import af.gov.anar.ebreshna.configuration.billing.model.VoltageGroupMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VoltageGroupRepository extends JpaRepository<VoltageGroup, Long> {
+public interface VoltageGroupRepository extends JpaRepository<VoltageGroupMaster, Long> {
 }
