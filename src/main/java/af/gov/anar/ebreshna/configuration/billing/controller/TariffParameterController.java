c50789a8eba11cddc208e15ed7e5e99694adf957
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 11:42:52 +0430

    #2 billing controller: tariff parameter

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/controller/TariffParameterController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/controller/TariffParameterController.java
@@ -1,10 +1,56 @@
 package af.gov.anar.ebreshna.configuration.billing.controller;
 
 import af.gov.anar.api.handler.ResponseHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.configuration.billing.model.CustomerGroupMaster;
+import af.gov.anar.ebreshna.configuration.billing.model.TariffParameter;
+import af.gov.anar.ebreshna.configuration.billing.service.CustomerGroupService;
+import af.gov.anar.ebreshna.configuration.billing.service.TariffParameterService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/config/billing/tariff-parameters")
 public class TariffParameterController extends ResponseHandler {
+
+    @Autowired
+    private TariffParameterService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<TariffParameter>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<TariffParameter> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<TariffParameter> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) TariffParameter obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<TariffParameter> save(@RequestBody(required = true) TariffParameter obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
