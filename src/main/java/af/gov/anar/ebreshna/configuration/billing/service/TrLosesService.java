7398f821c57e69f88efca872b88f7e7912236354
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 10:48:32 +0430

    #2 billing service: tariff parameter

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/service/TrLosesService.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/service/TrLosesService.java
@@ -1,4 +1,46 @@
 package af.gov.anar.ebreshna.configuration.billing.service;
 
+import af.gov.anar.ebreshna.configuration.billing.model.CustomerGroupMaster;
+import af.gov.anar.ebreshna.configuration.billing.model.TrLossesConfiguration;
+import af.gov.anar.ebreshna.configuration.billing.repository.CustomerGroupRepository;
+import af.gov.anar.ebreshna.configuration.billing.repository.TrLosesRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class TrLosesService {
+
+    @Autowired
+    private TrLosesRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public TrLossesConfiguration save(TrLossesConfiguration obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<TrLossesConfiguration> findall()
+    {
+        return repository.findAll();
+    }
+
+    public TrLossesConfiguration findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        TrLossesConfiguration obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
 }
