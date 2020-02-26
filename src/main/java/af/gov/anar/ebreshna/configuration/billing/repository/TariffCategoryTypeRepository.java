79572d7c2d448dd9cdb1e669cb35a83a5a85e41a
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 10:23:33 +0430

    #3 billing repository: tariff category type configuration

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/repository/TariffCategoryTypeRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/repository/TariffCategoryTypeRepository.java
@@ -1,4 +1,11 @@
 package af.gov.anar.ebreshna.configuration.billing.repository;
 
-public interface TariffCategoryTypeRepository {
+import af.gov.anar.ebreshna.configuration.billing.model.TariffCategoryTypeMaster;
+import af.gov.anar.ebreshna.configuration.billing.model.TrLossesConfiguration;
+import af.gov.anar.ebreshna.configuration.common.tariff.model.TariffCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TariffCategoryTypeRepository extends JpaRepository<TariffCategoryTypeMaster, Long> {
 }
