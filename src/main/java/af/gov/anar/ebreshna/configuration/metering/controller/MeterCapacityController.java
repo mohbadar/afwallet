410ede181cc6b7ec24e35ebff8adb5aed724bd4e
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 09:06:22 +0430

    metering endpoints

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/controller/MeterCapacityController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/controller/MeterCapacityController.java
@@ -1,4 +1,9 @@
 package af.gov.anar.ebreshna.configuration.metering.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/config/metering/meter-capacities")
 public class MeterCapacityController {
 }
--- src/main/java/af/gov/anar/ebreshna/configuration/metering/controller/ZoneCycleRelationController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/controller/ZoneCycleRelationController.java
@@ -1,4 +1,9 @@
 package af.gov.anar.ebreshna.configuration.metering.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/config/metering/zone-cycle-relations")
 public class ZoneCycleRelationController {
 }
