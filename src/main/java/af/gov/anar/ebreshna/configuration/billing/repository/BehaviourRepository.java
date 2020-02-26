bfc529f5ed2f9af5955f29d51fe1dbf3fbfcdb20
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 10:19:46 +0430

    #3 billing repository: behaviour config

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/repository/BehaviourRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/repository/BehaviourRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.billing.repository;
 
-public interface BehaviourRepository {
+import af.gov.anar.ebreshna.configuration.billing.model.BehaviourConfiguration;
+import af.gov.anar.ebreshna.configuration.billing.model.TrLossesConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BehaviourRepository extends JpaRepository<BehaviourConfiguration, Long> {
 }
--- src/main/java/af/gov/anar/ebreshna/configuration/billing/repository/TrLosesRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/repository/TrLosesRepository.java
@@ -1,4 +1,9 @@
 package af.gov.anar.ebreshna.configuration.billing.repository;
 
-public interface TrLosesRepository {
+import af.gov.anar.ebreshna.configuration.billing.model.TrLossesConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrLosesRepository extends JpaRepository<TrLossesConfiguration, Long> {
 }
