228aeab072c455d18d905260737a9c0d56ced9a9
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 09:19:34 +0430

    metering endpoint: meter make serial controller

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/controller/MeterMakeSerialController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/controller/MeterMakeSerialController.java
@@ -1,9 +1,58 @@
 package af.gov.anar.ebreshna.configuration.metering.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.api.handler.ResponseHandler;
+import af.gov.anar.ebreshna.configuration.metering.model.CycleConfiguration;
+import af.gov.anar.ebreshna.configuration.metering.model.MeterMakeSerial;
+import af.gov.anar.ebreshna.configuration.metering.service.CycleService;
+import af.gov.anar.ebreshna.configuration.metering.service.MeterMakeSerialService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import io.micrometer.core.instrument.Meter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/config/metering/meter-make-serials")
-public class MeterMakeSerialController {
+public class MeterMakeSerialController extends ResponseHandler {
+
+    @Autowired
+    private MeterMakeSerialService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<MeterMakeSerial>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<MeterMakeSerial> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<MeterMakeSerial> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) MeterMakeSerial obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<MeterMakeSerial> save(@RequestBody(required = true) MeterMakeSerial obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
+
 }
