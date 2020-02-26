7e1187a40bbb410a5c569982ea78125a15d8e15a
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 11:10:22 +0430

    #2 billing controller: response handlers

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/controller/BillDueDayController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/controller/BillDueDayController.java
@@ -1,9 +1,10 @@
 package af.gov.anar.ebreshna.configuration.billing.controller;
 
+import af.gov.anar.api.handler.ResponseHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/config/billing/bill-due-days")
-public class BillDueDayController {
+public class BillDueDayController extends ResponseHandler {
 }
--- src/main/java/af/gov/anar/ebreshna/configuration/billing/controller/TariffChargeDetailController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/controller/TariffChargeDetailController.java
@@ -1,9 +1,10 @@
 package af.gov.anar.ebreshna.configuration.billing.controller;
 
+import af.gov.anar.api.handler.ResponseHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/api/config/billing/tariff-charge-details")
-public class TariffChargeDetailController {
+public class TariffChargeDetailController extends ResponseHandler {
 }
