fb6c42aafa629055dd735fe26d10fb9aca546946
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 11:08:47 +0430

    #2 billing controller: endpoints

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/controller/TariffChargeController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/controller/TariffChargeController.java
@@ -1,4 +1,9 @@
 package af.gov.anar.ebreshna.configuration.billing.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/config/billing/tariff-charges")
 public class TariffChargeController {
 }
