30697f78b0bc5358bdd0c36b3b5b5b5b34f7f41d
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 10:24:49 +0430

    #3 billing repository: tariff charges

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/repository/TariffChargeRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/repository/TariffChargeRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.billing.repository;
 
-public interface TariffChargeRepository {
+import af.gov.anar.ebreshna.configuration.billing.model.TariffCharge;
+import af.gov.anar.ebreshna.configuration.billing.model.TrLossesConfiguration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TariffChargeRepository extends JpaRepository<TariffCharge, Long> {
 }
