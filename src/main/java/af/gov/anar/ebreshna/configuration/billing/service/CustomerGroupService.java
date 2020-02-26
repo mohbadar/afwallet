6837b72ee2adc5afbdaeee963087b57ca671406d
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 10:34:14 +0430

    #2 billing service: custoemr group

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/service/CustomerGroupService.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/service/CustomerGroupService.java
@@ -1,4 +1,46 @@
 package af.gov.anar.ebreshna.configuration.billing.service;
 
+import af.gov.anar.ebreshna.configuration.billing.model.CustomerGroupMaster;
+import af.gov.anar.ebreshna.configuration.billing.repository.CustomerGroupRepository;
+import af.gov.anar.ebreshna.configuration.office.model.PremisesSubCategory;
+import af.gov.anar.ebreshna.configuration.office.repository.PremisesSubCategoryRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class CustomerGroupService {
+
+    @Autowired
+    private CustomerGroupRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public CustomerGroupMaster save(CustomerGroupMaster obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<CustomerGroupMaster> findall()
+    {
+        return repository.findAll();
+    }
+
+    public CustomerGroupMaster findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        CustomerGroupMaster obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
 }
