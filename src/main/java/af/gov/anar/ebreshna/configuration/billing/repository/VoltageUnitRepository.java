f627db7c76b10f99b6748f99fe70f22286506f54
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 10:25:53 +0430

    #3 billing repository: voltage unit

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/repository/VoltageUnitRepository.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/repository/VoltageUnitRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.configuration.billing.repository;
 
-public interface VoltageUnitRepository {
+import af.gov.anar.ebreshna.configuration.billing.model.TrLossesConfiguration;
+import af.gov.anar.ebreshna.configuration.billing.model.VoltageUnit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoltageUnitRepository extends JpaRepository<VoltageUnit, Long> {
 }
