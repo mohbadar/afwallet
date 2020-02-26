087de4721ef08e88a2907d1971d9bf210d6910e8
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 09:09:27 +0430

    metering endpoint: activity relation controller

--- src/main/java/af/gov/anar/ebreshna/configuration/metering/controller/ActivityRelationController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/metering/controller/ActivityRelationController.java
@@ -1,9 +1,60 @@
 package af.gov.anar.ebreshna.configuration.metering.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.configuration.common.BaseEntity;
+import af.gov.anar.ebreshna.configuration.common.province.Province;
+import af.gov.anar.ebreshna.configuration.common.province.ProvinceService;
+import af.gov.anar.ebreshna.configuration.metering.model.ActivityRelation;
+import af.gov.anar.ebreshna.configuration.metering.service.ActivityRelationService;
+import af.gov.anar.ebreshna.configuration.network.model.AreaMaster;
+import af.gov.anar.ebreshna.configuration.network.service.AreaMasterService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/config/metering/activity-relations")
-public class ActivityRelationController {
+public class ActivityRelationController extends BaseEntity {
+
+
+    @Autowired
+    private ActivityRelationService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<ActivityRelation>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<ActivityRelation> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<ActivityRelation> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) ActivityRelation obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<ActivityRelation> save(@RequestBody(required = true) ActivityRelation obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
+
 }
