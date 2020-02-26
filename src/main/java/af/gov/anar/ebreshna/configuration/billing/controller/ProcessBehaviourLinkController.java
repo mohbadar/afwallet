0be957561c1c0a6adb4e46b22acfa12893262393
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 11:34:31 +0430

    #2 billing controller: process behaviour configuration

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/controller/ProcessBehaviourLinkController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/controller/ProcessBehaviourLinkController.java
@@ -1,10 +1,56 @@
 package af.gov.anar.ebreshna.configuration.billing.controller;
 
 import af.gov.anar.api.handler.ResponseHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.configuration.billing.model.CustomerGroupMaster;
+import af.gov.anar.ebreshna.configuration.billing.model.ProcessBehaviourLinkConfiguration;
+import af.gov.anar.ebreshna.configuration.billing.service.CustomerGroupService;
+import af.gov.anar.ebreshna.configuration.billing.service.ProcessBehaviourLinkService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/config/billing/process-behaviour-link")
 public class ProcessBehaviourLinkController extends ResponseHandler {
+
+    @Autowired
+    private ProcessBehaviourLinkService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<ProcessBehaviourLinkConfiguration>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<ProcessBehaviourLinkConfiguration> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<ProcessBehaviourLinkConfiguration> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) ProcessBehaviourLinkConfiguration obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<ProcessBehaviourLinkConfiguration> save(@RequestBody(required = true) ProcessBehaviourLinkConfiguration obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
