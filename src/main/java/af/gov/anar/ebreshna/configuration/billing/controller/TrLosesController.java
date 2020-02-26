b1cef6732ed2c93288fd62c0c01e05725d83e133
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 11:44:33 +0430

    #2 billing controller: tr losses

--- src/main/java/af/gov/anar/ebreshna/configuration/billing/controller/TrLosesController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/billing/controller/TrLosesController.java
@@ -1,10 +1,56 @@
 package af.gov.anar.ebreshna.configuration.billing.controller;
 
 import af.gov.anar.api.handler.ResponseHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.configuration.billing.model.CustomerGroupMaster;
+import af.gov.anar.ebreshna.configuration.billing.model.TrLossesConfiguration;
+import af.gov.anar.ebreshna.configuration.billing.service.CustomerGroupService;
+import af.gov.anar.ebreshna.configuration.billing.service.TrLosesService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/config/billing/tr-losses")
 public class TrLosesController extends ResponseHandler {
+
+    @Autowired
+    private TrLosesService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<TrLossesConfiguration>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<TrLossesConfiguration> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<TrLossesConfiguration> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) TrLossesConfiguration obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<TrLossesConfiguration> save(@RequestBody(required = true) TrLossesConfiguration obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
