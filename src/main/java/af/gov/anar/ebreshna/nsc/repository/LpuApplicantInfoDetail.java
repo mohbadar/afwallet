e877a5d6b3fef29ca944391fa6184f67de5aa032
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-26 18:42:29 +0430

    nsc repo: lpu info detail

--- src/main/java/af/gov/anar/ebreshna/nsc/repository/LpuApplicantInfoDetail.java
+++ src/main/java/af/gov/anar/ebreshna/nsc/repository/LpuApplicantInfoDetail.java
@@ -1,4 +1,8 @@
 package af.gov.anar.ebreshna.nsc.repository;
 
-public interface LpuApplicantInfoDetail {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LpuApplicantInfoDetail extends JpaRepository<LpuApplicantInfoDetail, Long> {
 }
