ec5e97d1ffbac4930db6aa3cf0ff1fdf1b506b05
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-27 11:00:36 +0430

    nsc controller: endpoints

--- src/main/java/af/gov/anar/ebreshna/configuration/csc/customer/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package af.gov.anar.ebreshna.configuration.csc.customer;
-
-import af.gov.anar.ebreshna.configuration.csc.customer.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- src/main/java/af/gov/anar/ebreshna/nsc/controller/LpuApplicantInfoDetailController.java
+++ src/main/java/af/gov/anar/ebreshna/nsc/controller/LpuApplicantInfoDetailController.java
@@ -1,4 +1,9 @@
 package af.gov.anar.ebreshna.nsc.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/nsc/lpu-applicants")
 public class LpuApplicantInfoDetailController {
 }
