67afb25245bd20c9975bee7cf6baf1c3b7a886a9
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-27 10:53:01 +0430

    nsc service: applicant document

--- src/main/java/af/gov/anar/ebreshna/nsc/service/ApplicantDocumentService.java
+++ src/main/java/af/gov/anar/ebreshna/nsc/service/ApplicantDocumentService.java
@@ -1,4 +1,48 @@
 package af.gov.anar.ebreshna.nsc.service;
 
+import af.gov.anar.ebreshna.configuration.office.office.OfficeMaster;
+import af.gov.anar.ebreshna.configuration.office.office.OfficeMasterRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import af.gov.anar.ebreshna.nsc.model.Applicant;
+import af.gov.anar.ebreshna.nsc.model.ApplicantDocument;
+import af.gov.anar.ebreshna.nsc.repository.ApplicantDocumentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class ApplicantDocumentService {
+
+    @Autowired
+    private ApplicantDocumentRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public ApplicantDocument save(ApplicantDocument obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<ApplicantDocument> findall()
+    {
+        return repository.findAll();
+    }
+
+    public ApplicantDocument findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        ApplicantDocument obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
+
 }
