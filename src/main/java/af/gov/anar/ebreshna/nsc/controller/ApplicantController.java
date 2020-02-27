d517aef331c9461789e388189012038d34ea77f5
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-27 11:04:19 +0430

    nsc controller: applicant

--- src/main/java/af/gov/anar/ebreshna/nsc/controller/ApplicantController.java
+++ src/main/java/af/gov/anar/ebreshna/nsc/controller/ApplicantController.java
@@ -1,10 +1,56 @@
 package af.gov.anar.ebreshna.nsc.controller;
 
 import af.gov.anar.api.handler.ResponseHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.configuration.csc.approval_limit.ApprovalLimit;
+import af.gov.anar.ebreshna.configuration.csc.approval_limit.ApprovalLimitService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import af.gov.anar.ebreshna.nsc.model.Applicant;
+import af.gov.anar.ebreshna.nsc.service.ApplicantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/nsc/applicants")
 public class ApplicantController extends ResponseHandler {
+
+    @Autowired
+    private ApplicantService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<Applicant>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<Applicant> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<Applicant> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) Applicant obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<Applicant> save(@RequestBody(required = true) Applicant obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
