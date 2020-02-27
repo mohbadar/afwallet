79d794f233d8e9c734acec801cb846bc8a5d8479
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-27 11:03:33 +0430

    nsc controller: appliance applicant relation

--- src/main/java/af/gov/anar/ebreshna/nsc/controller/ApplicantApplianceRelationController.java
+++ src/main/java/af/gov/anar/ebreshna/nsc/controller/ApplicantApplianceRelationController.java
@@ -1,10 +1,58 @@
 package af.gov.anar.ebreshna.nsc.controller;
 
 import af.gov.anar.api.handler.ResponseHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.configuration.csc.approval_limit.ApprovalLimit;
+import af.gov.anar.ebreshna.configuration.csc.approval_limit.ApprovalLimitService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import af.gov.anar.ebreshna.nsc.model.Applicant;
+import af.gov.anar.ebreshna.nsc.model.ApplicantApplianceRelation;
+import af.gov.anar.ebreshna.nsc.service.ApplicantApplianceRelationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/nsc/applicant-appliance-relations")
 public class ApplicantApplianceRelationController extends ResponseHandler {
+
+    @Autowired
+    private ApplicantApplianceRelationService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<ApplicantApplianceRelation>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<ApplicantApplianceRelation> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<ApplicantApplianceRelation> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) ApplicantApplianceRelation obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<ApplicantApplianceRelation> save(@RequestBody(required = true) ApplicantApplianceRelation obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
+
 }
