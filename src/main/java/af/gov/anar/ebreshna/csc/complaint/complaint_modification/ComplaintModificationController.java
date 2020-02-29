2fcb5c8fa27e6dcaddcfc021bcf6261141cfd77d
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-29 09:31:29 +0430

    csc controller: complaint modification

--- src/main/java/af/gov/anar/ebreshna/csc/complaint/complaint_modification/ComplaintModificationController.java
+++ src/main/java/af/gov/anar/ebreshna/csc/complaint/complaint_modification/ComplaintModificationController.java
@@ -1,10 +1,55 @@
 package af.gov.anar.ebreshna.csc.complaint.complaint_modification;
 
 import af.gov.anar.api.handler.ResponseHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.helpdesk.model.Complaint;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import af.gov.anar.ebreshna.nsc.applicant.Applicant;
+import af.gov.anar.ebreshna.nsc.applicant.ApplicantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/csc/complaint-modifications")
 public class ComplaintModificationController extends ResponseHandler {
+
+    @Autowired
+    private ComplaintModificationService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<ComplaintModification>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<ComplaintModification> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<ComplaintModification> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) ComplaintModification obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<ComplaintModification> save(@RequestBody(required = true) ComplaintModification obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
