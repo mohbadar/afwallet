91f6819da93d0b37aa49cf1db7e643ff4e930e83
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-29 09:41:55 +0430

    ecall repo

--- src/main/java/af/gov/anar/ebreshna/csc/ecall/feedback/FeedbackRepository.java
+++ src/main/java/af/gov/anar/ebreshna/csc/ecall/feedback/FeedbackRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.csc.ecall.feedback;
 
-public interface FeedbackRepository {
+import af.gov.anar.ebreshna.csc.complaint.complaint_modification.ComplaintModification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.history.RevisionRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long>, RevisionRepository<Feedback, Long, Integer> {
 }
