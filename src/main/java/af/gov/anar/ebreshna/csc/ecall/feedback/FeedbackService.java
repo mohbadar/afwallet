0d6892e371c519f1f70e27f06ca7d41f30c84d5b
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-29 09:44:05 +0430

    ecall service: feedback

--- src/main/java/af/gov/anar/ebreshna/csc/ecall/feedback/FeedbackService.java
+++ src/main/java/af/gov/anar/ebreshna/csc/ecall/feedback/FeedbackService.java
@@ -1,4 +1,45 @@
 package af.gov.anar.ebreshna.csc.ecall.feedback;
 
+import af.gov.anar.ebreshna.csc.complaint.complaint_modification.ComplaintModification;
+import af.gov.anar.ebreshna.csc.complaint.complaint_modification.ComplaintModificationRepository;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class FeedbackService {
+
+    @Autowired
+    private FeedbackRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public Feedback save(Feedback obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<Feedback> findall()
+    {
+        return repository.findAll();
+    }
+
+    public Feedback findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        Feedback obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
+
 }
