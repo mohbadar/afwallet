56faf85a2cfd804eb20c8a4ef0343e0533f72920
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-29 09:41:33 +0430

    ecall repo: ivr call

--- src/main/java/af/gov/anar/ebreshna/csc/ecall/ivr_call/IvrCallRepository.java
+++ src/main/java/af/gov/anar/ebreshna/csc/ecall/ivr_call/IvrCallRepository.java
@@ -1,4 +1,10 @@
 package af.gov.anar.ebreshna.csc.ecall.ivr_call;
 
-public interface IvrCallRepository {
+import af.gov.anar.ebreshna.csc.complaint.complaint_modification.ComplaintModification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.history.RevisionRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IvrCallRepository extends JpaRepository<IvrCall, Long>, RevisionRepository<IvrCall, Long, Integer> {
 }
