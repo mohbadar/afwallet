ae43d0d69096dd4b3b4acbc97edbdf7ac618cb2d
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-29 09:26:40 +0430

    csc service: complaint modification

--- src/main/java/af/gov/anar/ebreshna/csc/complaint/complaint_modification/ComplaintModificationService.java
+++ src/main/java/af/gov/anar/ebreshna/csc/complaint/complaint_modification/ComplaintModificationService.java
@@ -1,4 +1,45 @@
 package af.gov.anar.ebreshna.csc.complaint.complaint_modification;
 
+import af.gov.anar.ebreshna.configuration.billing.behaviour.BehaviourConfiguration;
+import af.gov.anar.ebreshna.configuration.billing.behaviour.BehaviourRepository;
+import af.gov.anar.ebreshna.helpdesk.model.Complaint;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
 public class ComplaintModificationService {
+
+    @Autowired
+    private ComplaintModificationRepository repository;
+
+    @Autowired
+    private UserService userService;
+
+    public ComplaintModification save(ComplaintModification obj)
+    {
+        return repository.save(obj);
+    }
+
+    public List<ComplaintModification> findall()
+    {
+        return repository.findAll();
+    }
+
+    public ComplaintModification findOne(long id)
+    {
+        return repository.getOne(id);
+    }
+
+    public void delete(long id)
+    {
+        ComplaintModification obj = repository.getOne(id);
+        obj.setDeleted(true);
+        obj.setUserId(userService.getId());
+        obj.setDeletedAt(new Date());
+        save(obj);
+    }
 }
