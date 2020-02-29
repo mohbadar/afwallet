f63188a1286df50676e0dba97e214e857bc9c70a
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-29 09:46:29 +0430

    ecall controller: feedback

--- src/main/java/af/gov/anar/ebreshna/csc/ecall/feedback/FeedbackController.java
+++ src/main/java/af/gov/anar/ebreshna/csc/ecall/feedback/FeedbackController.java
@@ -1,10 +1,54 @@
 package af.gov.anar.ebreshna.csc.ecall.feedback;
 
 import af.gov.anar.api.handler.ResponseHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.csc.complaint.complaint_modification.ComplaintModification;
+import af.gov.anar.ebreshna.csc.complaint.complaint_modification.ComplaintModificationService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/csc/ecall/feedbacks")
 public class FeedbackController extends ResponseHandler {
+
+    @Autowired
+    FeedbackService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<Feedback>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<Feedback> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<Feedback> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) Feedback obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<Feedback> save(@RequestBody(required = true) Feedback obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
 }
