e8a0999e1e6900d23a9d46d8bd4aa54a15367b0d
mohbadar <mohammadbadarhashimi@gmail.com>
2020-02-29 09:47:14 +0430

    ecall controller: ivr call

--- src/main/java/af/gov/anar/ebreshna/csc/ecall/ivr_call/IvrCallController.java
+++ src/main/java/af/gov/anar/ebreshna/csc/ecall/ivr_call/IvrCallController.java
@@ -1,10 +1,56 @@
 package af.gov.anar.ebreshna.csc.ecall.ivr_call;
 
 import af.gov.anar.api.handler.ResponseHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import af.gov.anar.ebreshna.csc.complaint.complaint_modification.ComplaintModification;
+import af.gov.anar.ebreshna.csc.complaint.complaint_modification.ComplaintModificationService;
+import af.gov.anar.ebreshna.infrastructure.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/csc/ecall/ivr-calls")
 public class IvrCallController extends ResponseHandler {
+
+
+    @Autowired
+    private IvrCallService service;
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<List<IvrCall>> findall()
+    {
+        return ResponseEntity.ok(service.findall());
+    }
+
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<IvrCall> findOne(@PathVariable(name = "id", required = true) long id)
+    {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<IvrCall> update(@PathVariable(name = "id", required = true) long id, @RequestBody(required = true) IvrCall obj)
+    {
+        obj.setId(id);
+        return ResponseEntity.ok(service.save(obj));
+    }
+
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody
+    ResponseEntity<IvrCall> save(@RequestBody(required = true) IvrCall obj)
+    {
+        return ResponseEntity.ok(service.save(obj));
+    }
+
 }
