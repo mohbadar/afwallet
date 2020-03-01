65ade6ec046c584c5bbc1a223d2d223d44f6d546
mohbadar <mohammadbadarhashimi@gmail.com>
2020-03-01 10:13:11 +0430

    enum data type

--- src/main/java/af/gov/anar/ebreshna/configuration/network/enumeration/SubstationCapacity.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/network/enumeration/SubstationCapacity.java
@@ -3,19 +3,16 @@ package af.gov.anar.ebreshna.configuration.network.enumeration;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
 public enum SubstationCapacity {
 
 
-    SUB_400_200_KV("400/200KV"),
-    SUB_200_132_KV("200/132KV"),
-    SUB_132_33_KV("142/33KV"),
-    SUB_220_132_33_KV("220/132/33KV"),
-    SUB_33_11_KV("33/11KV");
+    SUB_400_200_KV,
+    SUB_200_132_KV,
+    SUB_132_33_KV,
+    SUB_220_132_33_KV,
+    SUB_33_11_KV;
 
 
-    private String content;
 
 
 }
--- src/main/java/af/gov/anar/ebreshna/configuration/network/substation/SubstationMaster.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/network/substation/SubstationMaster.java
@@ -4,7 +4,10 @@ import af.gov.anar.ebreshna.configuration.common.BaseEntity;
 import af.gov.anar.ebreshna.configuration.network.enumeration.SubstationCapacity;
 import af.gov.anar.ebreshna.configuration.office.office.OfficeMaster;
 import af.gov.anar.ebreshna.infrastructure.util.Schema;
+import com.vladmihalcea.hibernate.type.basic.PostgreSQLEnumType;
 import lombok.*;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
 import org.hibernate.envers.Audited;
 import org.hibernate.envers.RelationTargetAuditMode;
 
@@ -20,15 +23,20 @@ import javax.persistence.*;
 @EqualsAndHashCode
 @ToString
 @Audited
+//@TypeDef(
+//        name = "pgsql_enum",
+//        typeClass = PostgreSQLEnumType.class
+//)
 public class SubstationMaster extends BaseEntity {
 
     @ManyToOne(targetEntity = OfficeMaster.class)
     @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
     private OfficeMaster officeMaster;
 
-    @Column(name="substation_capacity")
-    @Enumerated(EnumType.STRING)
-    private SubstationCapacity substationCapacity;
+//    @Enumerated(EnumType.STRING)
+//    @Column(columnDefinition = "substation_status_info")
+//    @Type( type = "pgsql_enum" )
+    private String substationCapacity;
 
     @Column
     private String stationName;
