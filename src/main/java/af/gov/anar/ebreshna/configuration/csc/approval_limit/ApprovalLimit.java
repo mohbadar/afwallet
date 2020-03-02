44428eaef54fbb5d6a29d11592b839e0c3ed66ab
mohbadar <mohammadbadarhashimi@gmail.com>
2020-03-02 14:01:31 +0430

    csc: approvallimit: designation

--- src/main/java/af/gov/anar/ebreshna/configuration/csc/approval_limit/ApprovalLimit.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/csc/approval_limit/ApprovalLimit.java
@@ -2,6 +2,7 @@ package af.gov.anar.ebreshna.configuration.csc.approval_limit;
 
 import af.gov.anar.ebreshna.configuration.common.BaseEntity;
 import af.gov.anar.ebreshna.configuration.common.province.Province;
+import af.gov.anar.ebreshna.configuration.office.designation.DesignationMaster;
 import lombok.*;
 import org.hibernate.envers.Audited;
 import org.hibernate.envers.RelationTargetAuditMode;
@@ -21,14 +22,20 @@ import java.math.BigDecimal;
 @Audited
 public class ApprovalLimit extends BaseEntity {
 
-    @ManyToOne(targetEntity = ApprovalLimit.class, fetch = FetchType.EAGER)
-    @JoinColumn(nullable = true, name = "approval_limit_id")
-    private ApprovalLimit approvalLimit;
+//    @ManyToOne(targetEntity = ApprovalLimit.class, fetch = FetchType.EAGER)
+//    @JoinColumn(nullable = true, name = "approval_limit_id")
+//    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+//    private ApprovalLimit approvalLimit;
 
     @ManyToOne(targetEntity = Province.class, fetch = FetchType.EAGER)
     @JoinColumn(nullable = true, name = "province_id")
     @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
     private Province province;
+
+    @ManyToOne(targetEntity = DesignationMaster.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = true, name = "designation_master_id")
+    @Audited(targetAuditMode = RelationTargetAuditMode.NOT_AUDITED)
+    private DesignationMaster designationMaster;
 
     private BigDecimal amount;
 }
