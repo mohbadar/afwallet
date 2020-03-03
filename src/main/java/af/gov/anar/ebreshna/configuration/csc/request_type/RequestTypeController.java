bc16282576bc90df4eccdfdc22fbc691e9b1e19a
Ahmad Faiaz <ahmadfaiaz22@gmail.com>
2020-03-03 16:42:40 +0430

    csc request type path

--- src/main/java/af/gov/anar/ebreshna/configuration/csc/request_type/RequestTypeController.java
+++ src/main/java/af/gov/anar/ebreshna/configuration/csc/request_type/RequestTypeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/config/csc/requesttypes")
+@RequestMapping(value = "/api/config/csc/request_types")
 public class RequestTypeController {
 
     @Autowired
