cb6aaabb76fc715d36a67d5efc14aa11727bf093
mohbadar <mohammadbadarhashimi@gmail.com>
2020-03-07 10:50:34 +0430

    mail kafka producer

---
+++ src/main/java/af/gov/anar/ebreshna/notification/mail/producer/MailKafkaProducer.java
@@ -0,0 +1,21 @@
+package af.gov.anar.ebreshna.notification.mail.producer;
+
+import af.gov.anar.ebreshna.notification.mail.model.Mail;
+import af.gov.anar.ebreshna.notification.mail.util.Constant;
+import af.gov.anar.lib.kafka.producer.AnarKafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+
+@Service
+public class MailKafkaProducer {
+
+    @Autowired
+    private AnarKafkaProducer<Mail> anarKafkaProducer;
+
+    public ListenableFuture<SendResult<String, Mail>> produce(Mail obj)
+    {
+        return anarKafkaProducer.produce(Constant.MAIL_KAFKA_TOPIC, obj);
+    }
+}
