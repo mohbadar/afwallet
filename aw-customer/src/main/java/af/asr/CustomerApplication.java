124925747afa767ebbd786132772d5f71e075a80
mohbadar <mohammadbadarhashimi@gmail.com>
2020-03-14 15:38:26 +0430

    catalog manager

--- aw-customer/src/main/java/af/asr/CustomerApplication.java
+++ aw-customer/src/main/java/af/asr/CustomerApplication.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -22,6 +23,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableAsync
 @EnableRetry
 @Slf4j
+@EnableFeignClients
 public class CustomerApplication {
 
 	public static void main(String[] args) {
