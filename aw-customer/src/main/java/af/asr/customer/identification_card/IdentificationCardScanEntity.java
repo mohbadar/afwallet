83213856193234205e91fc6e8f0d12d4dba4a7c2
mohbadar <mohammadbadarhashimi@gmail.com>
2020-03-14 09:56:59 +0430

    identification card scan entity

--- aw-customer/src/main/java/af/asr/customer/identification_card/IdentificationCardScanEntity.java
+++ aw-customer/src/main/java/af/asr/customer/identification_card/IdentificationCardScanEntity.java
@@ -1,6 +1,5 @@
 package af.asr.customer.identification_card;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Convert;
@@ -10,41 +9,38 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import af.asr.customer.customer.CustomerEntity;
-
 @Entity
-@Table(name = "maat_identification_cards")
-public class IdentificationCardEntity {
+@Table(name = "identification_card_scan")
+public class IdentificationCardScanEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    @Column(name = "a_type")
-    private String type;
+    @Column(name = "identifier")
+    private String identifier;
+    @Column(name = "description")
+    private String description;
+    @Lob
+    @Column(name = "image")
+    private byte[] image;
+    @Column(name = "size")
+    private Long size;
+    @Column(name = "content_type")
+    private String contentType;
     @ManyToOne(fetch = FetchType.LAZY, optional = true)
-    @JoinColumn(name = "customer_id")
-    private CustomerEntity customer;
-    @Column(name = "a_number")
-    private String number;
-    @Column(name = "expiration_date")
-//    @Convert(converter = LocalDateConverter.class)
-    private LocalDate expirationDate;
-    @Column(name = "issuer")
-    private String issuer;
+    @JoinColumn(name = "identification_card_id")
+    private IdentificationCardEntity identificationCard;
     @Column(name = "created_by")
     private String createdBy;
     @Column(name = "created_on")
 //    @Convert(converter = LocalDateTimeConverter.class)
     private LocalDateTime createdOn;
-    @Column(name = "last_modified_by")
-    private String lastModifiedBy;
-    @Column(name = "last_modified_on")
-//    @Convert(converter = LocalDateTimeConverter.class)
-    private LocalDateTime lastModifiedOn;
+
 
 
 }
