e2f50fc19f616854262825a747b102667cec5683
mohbadar <mohammadbadarhashimi@gmail.com>
2020-03-14 10:05:47 +0430

    customer: repository: task instance

---
+++ aw-customer/src/main/java/af/asr/customer/task/repository/TaskInstanceRepository.java
@@ -0,0 +1,18 @@
+package af.asr.customer.task.repository;
+
+import af.asr.customer.customer.CustomerEntity;
+import af.asr.customer.task.model.TaskDefinitionEntity;
+import af.asr.customer.task.model.TaskInstanceEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskInstanceRepository extends JpaRepository<TaskInstanceEntity, Long> {
+
+    List<TaskInstanceEntity> findByCustomer(final CustomerEntity customer);
+
+    List<TaskInstanceEntity> findByCustomerAndTaskDefinition(final CustomerEntity customer,
+                                                             final TaskDefinitionEntity taskDefinitionEntity);
+}
