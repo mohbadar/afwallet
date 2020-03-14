30c327e63b60472d7b3cfb4e9f7b781e5abdfd5a
mohbadar <mohammadbadarhashimi@gmail.com>
2020-03-14 16:41:11 +0430

    domain entities

---
+++ aw-notification/src/main/java/af/asr/notification/domain/Template.java
@@ -0,0 +1,107 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package af.asr.notification.domain;
+
+import java.util.Objects;
+
+public class Template {
+	private String templateIdentifier;
+	private String subject;
+	private String senderEmail;
+	private String message;
+	private String url;
+	
+	public Template(String templateIdentifier, String senderEmail, String subject, String message, String url) {
+		this.templateIdentifier = templateIdentifier;
+		this.senderEmail = senderEmail;
+		this.subject = subject;
+		this.message = message;
+		this.url = url;
+	}
+	
+	public Template() {
+	}
+	
+	public String getTemplateIdentifier() {
+		return templateIdentifier;
+	}
+	
+	public void setTemplateIdentifier(String templateIdentifier) {
+		this.templateIdentifier = templateIdentifier;
+	}
+	
+	public String getSubject() {
+		return subject;
+	}
+	
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	
+	public String getSenderEmail() {
+		return senderEmail;
+	}
+	
+	public void setSenderEmail(String senderEmail) {
+		this.senderEmail = senderEmail;
+	}
+	
+	public String getMessage() {
+		return message;
+	}
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	@Override
+	public String toString() {
+		return "Template{" +
+				"templateIdentifier='" + templateIdentifier + '\'' +
+				", subject='" + subject + '\'' +
+				", senderEmail='" + senderEmail + '\'' +
+				", message='" + message + '\'' +
+				", url='" + url + '\'' +
+				'}';
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Template template = (Template) o;
+		return Objects.equals(templateIdentifier, template.templateIdentifier) &&
+				Objects.equals(subject, template.subject) &&
+				Objects.equals(message, template.message) &&
+				Objects.equals(url, template.url);
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(templateIdentifier, subject, message, senderEmail,url);
+	}
+}
---
+++ aw-notification/src/main/java/af/asr/notification/events/NotificationEventConstants.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package af.asr.notification.events;
+
+@SuppressWarnings("unused")
+public interface NotificationEventConstants {
+	
+	String DESTINATION = "notification-v1";
+	String SELECTOR_NAME = "action";
+	String INITIALIZE = "initialize";
+	
+	String POST_SMS_CONFIGURATION = "post-sms-configuration";
+	String POST_EMAIL_CONFIGURATION = "post-email-configuration";
+	String POST_TEMPLATE = "post-template";
+	String POST_SOURCE_APPLICATION = "post-source-application";
+	String UPDATE_SMS_CONFIGURATION = "update-sms-configuration";
+	String UPDATE_EMAIL_CONFIGURATION = "update-email-configuration";
+	String DELETE_SMS_CONFIGURATION = "delete-sms-configuration";
+	String DELETE_EMAIL_CONFIGURATION = "delete-email-configuration";
+	String DELETE_TEMPLATE = "delete-template";
+	String DELETE_SOURCE_APPLICATION = "delete-source-application";
+	
+	String SEND_EMAIL_NOTIFICATION = "post-send-email-notification";
+	String SEND_SMS_NOTIFICATION = "post-send-sms-notification";
+	
+	String POST_ENABLE_CUSTOMER_CREATED_EVENT = "post-enable-customer-created-event";
+	String POST_ENABLE_CUSTOMER_UPDATED_EVENT = "post-enable-customer-updated-event";
+	String POST_ENABLE_CUSTOMER_CLOSED_EVENT = "post-enable-customer-closed-event";
+	String POST_ENABLE_CUSTOMER_LOCKED_EVENT = "post-enable-customer-locked-event";
+	String POST_ENABLE_CUSTOMER_ACTIVATED_EVENT = "post-enable-customer-activated-event";
+	String POST_ENABLE_CUSTOMER__EVENT = "post-enable-customer--event";
+	
+	String SELECTOR_INITIALIZE = SELECTOR_NAME + " = '" + INITIALIZE + "'";
+	String SELECTOR_POST_SMS_CONFIGURATION = SELECTOR_NAME + " = '" + POST_SMS_CONFIGURATION + "'";
+	String SELECTOR_POST_EMAIL_CONFIGURATION = SELECTOR_NAME + " = '" + POST_EMAIL_CONFIGURATION + "'";
+	String SELECTOR_POST_TEMPLATE = SELECTOR_NAME + " = '" + POST_TEMPLATE + "'";
+	String SELECTOR_UPDATE_SMS_CONFIGURATION = SELECTOR_NAME + " = '" + UPDATE_SMS_CONFIGURATION + "'";
+	String SELECTOR_UPDATE_EMAIL_CONFIGURATION = SELECTOR_NAME + " = '" + UPDATE_EMAIL_CONFIGURATION + "'";
+	String SELECTOR_DELETE_SMS_CONFIGURATION = SELECTOR_NAME + " = '" + DELETE_SMS_CONFIGURATION + "'";
+	String SELECTOR_DELETE_EMAIL_CONFIGURATION = SELECTOR_NAME + " = '" + DELETE_EMAIL_CONFIGURATION + "'";
+	String SELECTOR_POST_SOURCE_APPLICATION = SELECTOR_NAME + " = '" + POST_SOURCE_APPLICATION + "'";
+	String SELECTOR_DELETE_SOURCE_APPLICATION = SELECTOR_NAME + " = '" + DELETE_SOURCE_APPLICATION + "'";
+	
+	String SELECTOR_SEND_EMAIL_NOTIFICATION = SELECTOR_NAME + " = '" + SEND_EMAIL_NOTIFICATION + "'";
+	String SELECTOR_SEND_SMS_NOTIFICATION = SELECTOR_NAME + " = '" + SEND_SMS_NOTIFICATION + "'";
+	
+	String TEST = "jms-test";
+	String SELECTOR_TEST = SELECTOR_NAME + " = '" + TEST + "'";
+}
