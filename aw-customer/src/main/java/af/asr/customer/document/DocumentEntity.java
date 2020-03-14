0d9207b8a8bef8d5ab99fe90151b2d12641c33d1
mohbadar <mohammadbadarhashimi@gmail.com>
2020-03-14 09:51:58 +0430

    document entity

---
+++ aw-customer/src/main/java/af/asr/customer/document/DocumentEntity.java
@@ -0,0 +1,48 @@
+package af.asr.customer.document;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import af.asr.customer.customer.CustomerEntity;
+
+@Entity
+@Table(name = "documents")
+public class DocumentEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "customer_id")
+    private CustomerEntity customer;
+
+    @Column(name = "identifier", nullable = false)
+    private String identifier;
+
+    @SuppressWarnings("DefaultAnnotationParam")
+    @Column(name = "description", nullable = true)
+    private String description;
+
+    @Column(name = "is_completed", nullable = false)
+    private Boolean completed;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "created_on")
+//    @Convert(converter = LocalDateTimeConverter.class)
+    private LocalDateTime createdOn;
+
+}
