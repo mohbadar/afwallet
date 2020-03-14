012a90170b5fe241852a236d07c858c04a64796a
mohbadar <mohammadbadarhashimi@gmail.com>
2020-03-14 10:00:16 +0430

    portrait  entity

---
+++ aw-customer/src/main/java/af/asr/customer/identification_card/PortraitEntity.java
@@ -0,0 +1,39 @@
+package af.asr.customer.identification_card;
+
+import af.asr.customer.customer.CustomerEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "portrait")
+public class PortraitEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "customer_id")
+    private CustomerEntity customer;
+
+    @Lob
+    @Column(name = "image")
+    private byte[] image;
+
+    @Column(name = "size")
+    private Long size;
+
+    @Column(name = "content_type")
+    private String contentType;
+
+}
